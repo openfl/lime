4068f30c99f89d316533b50dbc1dcde9fbf732c5
underscorediscovery <fuzzyspoon@gmail.com>
2013-06-25 03:14:41 -0230

    Adding some minor tweaks

--- nmegl/utils/IMemoryRange.hx
+++ nmegl/utils/IMemoryRange.hx
@@ -6,10 +6,8 @@ import nmegl.utils.ByteArray;
 
 interface IMemoryRange {
     
-    
     public function getByteBuffer ():ByteArray;
     public function getStart ():Int;
     public function getLength ():Int;
-    
-    
+
 }
