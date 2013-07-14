7ccb65ed27a7f96a3594d9e0ad2f58722a8ce2cc
underscorediscovery <sven@underscorediscovery.com>
2013-07-14 07:09:12 -0230

    Adding keys

---
+++ lime/utils/Keys.hx
@@ -0,0 +1,32 @@
+package lime.utils;
+
+class Keys {
+
+    public var key_A : Int = 97;
+    public var key_B : Int = 98;
+    public var key_C : Int = 99;
+    public var key_D : Int = 100;
+    public var key_E : Int = 101;
+    public var key_F : Int = 102;
+    public var key_G : Int = 103;
+    public var key_H : Int = 104;
+    public var key_I : Int = 105;
+    public var key_J : Int = 106;
+    public var key_K : Int = 107;
+    public var key_L : Int = 108;
+    public var key_M : Int = 109;
+    public var key_N : Int = 110;
+    public var key_O : Int = 111;
+    public var key_P : Int = 112;
+    public var key_Q : Int = 113;
+    public var key_R : Int = 114;
+    public var key_S : Int = 115;
+    public var key_T : Int = 116;
+    public var key_U : Int = 117;
+    public var key_V : Int = 118;
+    public var key_X : Int = 119;
+    public var key_Y : Int = 120;
+    public var key_Z : Int = 121;
+
+    public function new() {}
+}
