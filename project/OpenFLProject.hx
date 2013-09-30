10e65b7e65e19f56406e3e18b6fddd89bf49bf4f
Joshua Granick <joshua@joshuagranick.com>
2013-09-30 11:29:14 -0700

    Default to ARMv5/ARMv6 for Android

--- project/OpenFLProject.hx
+++ project/OpenFLProject.hx
@@ -116,7 +116,7 @@ class OpenFLProject {
 				
 				platformType = PlatformType.MOBILE;
 				
-				if (target == Platform.IOS || target == Platform.ANDROID) {
+				if (target == Platform.IOS) {
 					
 					architectures = [ Architecture.ARMV7 ];
 					
