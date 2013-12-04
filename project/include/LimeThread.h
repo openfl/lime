f990832507323adca2e14125ca92132a7b131cd7
underscorediscovery <sven@underscorediscovery.com>
2013-12-04 07:39:53 -0330

    tidy; General tidying of code.

--- project/include/LimeThread.h
+++ project/include/LimeThread.h
@@ -1,32 +1,36 @@
 #ifndef LIME_THREAD_H
 #define LIME_THREAD_H
 
-#ifndef HX_WINDOWS
-#ifndef EPPC
-#include <pthread.h>
-#endif
-#else
-#include <windows.h>
-#undef min
-#undef max
-#endif
 
-namespace lime
-{
-#ifndef HX_WINDOWS
-#ifdef EPPC
-typedef int ThreadId;
-#else
-typedef pthread_t ThreadId;
-#endif
-#else
-typedef DWORD ThreadId;
-#endif
+    #ifndef HX_WINDOWS
+        #ifndef EPPC
+            #include <pthread.h>
+        #endif
+    #else
+        #include <windows.h>
+        #undef min
+        #undef max
+    #endif
 
-ThreadId GetThreadId();
-bool IsMainThread();
-void SetMainThread();
-}
+namespace lime {
 
-#endif
 
+    #ifndef HX_WINDOWS
+        #ifdef EPPC
+            typedef int ThreadId;
+        #else
+            typedef pthread_t ThreadId;
+        #endif
+    #else
+        typedef DWORD ThreadId;
+    #endif
+
+    ThreadId    GetThreadId();
+    bool        IsMainThread();
+    void        SetMainThread();
+
+
+} //namespace lime
+
+
+#endif //LIME_THREAD_H
--- project/src/platform/mac/System.mm
+++ project/src/platform/mac/System.mm
@@ -5,71 +5,86 @@
 #include <sys/types.h>
 #include <sys/sysctl.h>
 
+
 namespace lime {
 
-bool LaunchBrowser(const char *inUtf8URL)
-{
-	#ifndef OBJC_ARC
-	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	#endif
-	NSString *str = [[NSString alloc] initWithUTF8String:inUtf8URL];
-	[[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString: str]];
-	#ifndef OBJC_ARC
-	[str release];
-	[pool drain];
-	#endif
-	return true;
-}
+	bool LaunchBrowser(const char *inUtf8URL) {
 
-std::string CapabilitiesGetLanguage()
-{
-	#ifndef OBJC_ARC
-	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	#endif
-	NSString *language = [[NSLocale preferredLanguages] objectAtIndex:0];
-	std::string result = (language?[language UTF8String]:"");
-	#ifndef OBJC_ARC
-	[pool drain];
-	#endif
-	return result;
-}
+		#ifndef OBJC_ARC
+			NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+		#endif
 
-double CapabilitiesGetScreenDPI()
-{
-	#ifndef OBJC_ARC
-	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	#endif
-	
-	NSScreen *screen = [NSScreen mainScreen];
-	NSDictionary *description = [screen deviceDescription];
-	NSSize displayPixelSize = [[description objectForKey:NSDeviceSize] sizeValue];
-	CGSize displayPhysicalSize = CGDisplayScreenSize(
-	            [[description objectForKey:@"NSScreenNumber"] unsignedIntValue]);
-	double result = ((displayPixelSize.width / displayPhysicalSize.width) + (displayPixelSize.height / displayPhysicalSize.height)) * 0.5 * 25.4;
-	
-	#ifndef OBJC_ARC
-	[pool drain];
-	#endif
-	return result;
-}
+		NSString *str = [[NSString alloc] initWithUTF8String:inUtf8URL];
+		[[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString: str]];
+		
+		#ifndef OBJC_ARC
+			[str release];
+			[pool drain];
+		#endif
 
-double CapabilitiesGetPixelAspectRatio() {
-	#ifndef OBJC_ARC
-	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	#endif
-	
-	NSScreen *screen = [NSScreen mainScreen];
-	NSDictionary *description = [screen deviceDescription];
-	NSSize displayPixelSize = [[description objectForKey:NSDeviceSize] sizeValue];
-	CGSize displayPhysicalSize = CGDisplayScreenSize(
-	            [[description objectForKey:@"NSScreenNumber"] unsignedIntValue]);
-	double result = (displayPixelSize.width / displayPhysicalSize.width) / (displayPixelSize.height / displayPhysicalSize.height);
-	
-	#ifndef OBJC_ARC
-	[pool drain];
-	#endif
-	return result;
-}
+		return true;
+
+	}
+
+	std::string CapabilitiesGetLanguage() {
+
+		#ifndef OBJC_ARC
+			NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+		#endif
+
+		NSString *language = [[NSLocale preferredLanguages] objectAtIndex:0];
+		std::string result = (language?[language UTF8String]:"");
+		
+		#ifndef OBJC_ARC
+			[pool drain];
+		#endif
+
+		return result;
+
+	}
+
+	double CapabilitiesGetScreenDPI() {
+
+		#ifndef OBJC_ARC
+			NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+		#endif
+		
+		NSScreen *screen = [NSScreen mainScreen];
+		NSDictionary *description = [screen deviceDescription];
+		NSSize displayPixelSize = [[description objectForKey:NSDeviceSize] sizeValue];
+		CGSize displayPhysicalSize = 
+				CGDisplayScreenSize( [[description objectForKey:@"NSScreenNumber"] unsignedIntValue] );
+		double result = ((displayPixelSize.width / displayPhysicalSize.width) + (displayPixelSize.height / displayPhysicalSize.height)) * 0.5 * 25.4;
+		
+		#ifndef OBJC_ARC
+			[pool drain];
+		#endif
+
+		return result;
+
+	}
+
+	double CapabilitiesGetPixelAspectRatio() {
+
+		#ifndef OBJC_ARC
+			NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+		#endif
+		
+		NSScreen *screen = [NSScreen mainScreen];
+		NSDictionary *description = [screen deviceDescription];
+		NSSize displayPixelSize = [[description objectForKey:NSDeviceSize] sizeValue];
+		CGSize displayPhysicalSize = 
+				CGDisplayScreenSize( [[description objectForKey:@"NSScreenNumber"] unsignedIntValue] );
+
+		double result = (displayPixelSize.width / displayPhysicalSize.width) / (displayPixelSize.height / displayPhysicalSize.height);
+		
+		#ifndef OBJC_ARC
+			[pool drain];
+		#endif
+
+		return result;
+
+	}
 
 	std::string FileDialogOpen( const std::string &title, const std::string &text, const std::vector<std::string> &fileTypes ) {
 	    
