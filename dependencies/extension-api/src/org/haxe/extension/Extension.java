94d58bfc9a74f88d3cd8516077586cec77323999
sergey-miryanov <sergey.miryanov@gmail.com>
2014-01-02 15:24:41 +0600

    Fix import of View in Extension.java

--- dependencies/extension-api/src/org/haxe/extension/Extension.java
+++ dependencies/extension-api/src/org/haxe/extension/Extension.java
@@ -7,7 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.ViewGroup;
+import android.view.View;
 
 
 public class Extension {
