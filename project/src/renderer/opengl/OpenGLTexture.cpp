7ab09534395a6187317251fd4320f83765489fed
Joshua Granick <joshua@joshuagranick.com>
2014-01-24 08:31:11 -0800

    Fix software fallback on some older/more minimal graphic cards

--- project/src/renderer/opengl/OpenGLTexture.cpp
+++ project/src/renderer/opengl/OpenGLTexture.cpp
@@ -182,6 +182,10 @@ namespace lime {
 			free (buffer);
 		
 		mSmooth = true;
+		
+		#ifndef LIME_GLES
+		glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
+		#endif
 		glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		
