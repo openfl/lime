aab783bef0e69ba058a2809075adfe773ccd3e01
Joshua Granick <joshua@joshuagranick.com>
2014-04-10 13:20:50 -0700

    HXCPP fix

--- script/src/RunScript.hx
+++ script/src/RunScript.hx
@@ -121,10 +121,10 @@ class RunScript {
 				var directories = [ PathHelper.combine (path, "obj") ];
 				var files = [ PathHelper.combine (path, "all_objs"), PathHelper.combine (path, "vc100.pdb"), PathHelper.combine (path, "vc110.pdb") ];
 				
-				if (PathHelper.getHaxelib (new Haxelib ("lime-wiiu")) != "") {
+				if (PathHelper.getHaxelib (new Haxelib ("openfl-wiiu")) != "") {
 					
-					directories.push (PathHelper.combine (PathHelper.getHaxelib (new Haxelib ("lime-wiiu")), "project/obj"));
-					directories.push (PathHelper.combine (PathHelper.getHaxelib (new Haxelib ("lime-wiiu")), "project/all_objs"));
+					directories.push (PathHelper.combine (PathHelper.getHaxelib (new Haxelib ("openfl-wiiu")), "project/obj"));
+					directories.push (PathHelper.combine (PathHelper.getHaxelib (new Haxelib ("openfl-wiiu")), "project/all_objs"));
 					
 				}
 				
@@ -231,7 +231,7 @@ class RunScript {
 		
 		if (target == "wiiu" && path == PathHelper.combine (limeDirectory, "project")) {
 			
-			path = PathHelper.combine (PathHelper.getHaxelib (new Haxelib ("lime-wiiu"), true), "project");
+			path = PathHelper.combine (PathHelper.getHaxelib (new Haxelib ("openfl-wiiu"), true), "project");
 			
 		}
 		
@@ -597,19 +597,19 @@ class RunScript {
 				
 				if (buildSharedLibs) {
 					
-					runCommand (sharedLibsPath, "neko", [ "build.n", "wiiu" ].concat (defines));
+					//runCommand (sharedLibsPath, "neko", [ "build.n", "wiiu" ].concat (defines));
 					
 				}
 				
 				if (!flags.exists ("debug")) {
 					
-					runCommand (path, "haxelib", [ "run", buildLib, buildFile, "-Dnintendo", "-Dwiiu", "-Dtoolchain=wiiu", "-I" + PathHelper.getHaxelib (new Haxelib ("lime-wiiu")) + "/toolchain" ].concat (defines));
+					runCommand (path, "haxelib", [ "run", buildLib, buildFile, "-Dnintendo", "-Dwiiu", "-Dtoolchain=wiiu", "-I" + PathHelper.getHaxelib (new Haxelib ("openfl-wiiu")) ].concat (defines));
 					
 				}
 				
 				if (!flags.exists ("release")) {
 					
-					runCommand (path, "haxelib", [ "run", buildLib, buildFile, "-Dnintendo", "-Dwiiu", "-Dfulldebug", "-Dtoolchain=wiiu", "-I" + PathHelper.getHaxelib (new Haxelib ("lime-wiiu")) + "/toolchain" ].concat (defines));
+					runCommand (path, "haxelib", [ "run", buildLib, buildFile, "-Dnintendo", "-Dwiiu", "-Dfulldebug", "-Dtoolchain=wiiu", "-I" + PathHelper.getHaxelib (new Haxelib ("openfl-wiiu")) ].concat (defines));
 					
 				}
 			
