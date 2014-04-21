1e1b2330156093057e7bb9876c08cd9502611ef6
Joshua Granick <joshua@joshuagranick.com>
2014-04-20 20:22:16 -0700

    Progress on new config system

--- helpers/ObjectHelper.hx
+++ helpers/ObjectHelper.hx
@@ -1,6 +1,10 @@
 package helpers;
 
 
+import haxe.ds.IntMap;
+import haxe.ds.StringMap;
+
+
 @:generic class ObjectHelper {
 	
 	
@@ -49,6 +53,95 @@ package helpers;
 		}
 		
 		return destination;
+		
+	}
+	
+	
+	public static function deepCopy<T> (v:T):T {
+		
+		if (!Reflect.isObject (v)) { // simple type
+			
+			return v;
+			
+		} else if (Std.is (v, String)) { // string
+			
+			return v;
+			
+		} else if (Std.is (v, Array)) { // array
+			
+			var result = Type.createInstance (Type.getClass (v), []);
+			
+			untyped {
+				for (ii in 0...v.length) {
+					result.push (deepCopy (v[ii]));
+				}
+			}
+			
+			return result;
+			
+		} else if (Std.is (v, StringMap)) { // hashmap
+			
+			var result = Type.createInstance (Type.getClass(v), []);
+			
+			untyped {
+				var keys:Iterator<String> = v.keys ();
+				for (key in keys) {
+					result.set (key, deepCopy (v.get (key)));
+				}
+			}
+			
+			return result;
+			
+		} else if (Std.is (v, IntMap)) { // integer-indexed hashmap
+			
+			var result = Type.createInstance (Type.getClass (v), []);
+			
+			untyped {
+				var keys:Iterator<Int> = v.keys ();
+				for (key in keys) {
+					result.set (key, deepCopy (v.get (key)));
+				}
+			}
+			 
+			return result;
+			
+		} else if (Std.is (v, List)) { // list
+			
+			//List would be copied just fine without this special case, but I want to avoid going recursive
+			var result = Type.createInstance (Type.getClass (v), []);
+			
+			untyped {
+				var iter:Iterator<Dynamic> = v.iterator ();
+				for (ii in iter) {
+					result.add (ii);
+				}
+			}
+			
+			return result;
+			
+		} else if(Type.getClass(v) == null) { // anonymous object
+			
+			var obj:Dynamic = {};
+			
+			for (ff in Reflect.fields (v)) { 
+				Reflect.setField (obj, ff, deepCopy (Reflect.field (v, ff))); 
+			}
+			
+			return obj;
+			
+		} else { // class
+			
+			var obj = Type.createEmptyInstance (Type.getClass (v));
+			 
+			for (ff in Reflect.fields (v)) {
+				Reflect.setField (obj, ff, deepCopy (Reflect.field (v, ff))); 
+			}
+			
+			return obj;
+			
+		}
+		
+		return null;
 		
 	}
 	
---
+++ project/ConfigData.hx
@@ -0,0 +1,100 @@
+package project;
+
+
+import helpers.ObjectHelper;
+
+
+class ConfigData implements Dynamic {
+	
+	
+	public function new () {
+		
+		
+		
+	}
+	
+	
+	public function clone ():ConfigData {
+		
+		return ObjectHelper.deepCopy (this);
+		
+	}
+	
+	
+	public function get (type:String, id:String):Dynamic {
+		
+		var object = getObject (type, id);
+		var propertyName = getPropertyName (id);
+		
+		return Reflect.field (object, propertyName);
+		
+	}
+	
+	
+	private function getObject (type:String, id:String):Dynamic {
+		
+		var split = id.split (".");
+		var path = [ type ];
+		
+		if (split.length > 1) {
+			
+			path = path.concat (split.slice (0, split.length - 1));
+			
+		}
+		
+		var object = this;
+		
+		for (element in path) {
+			
+			if (!Reflect.hasField (object, element)) {
+				
+				return null;
+				
+			}
+			
+			object = Reflect.field (object, element);
+			
+		}
+		
+		return object;
+		
+	}
+	
+	
+	public function getPropertyName (id:String):String {
+		
+		if (id == null) return null;
+		
+		var split = id.split (".");
+		return split[split.length - 1];
+		
+	}
+	
+	
+	public function exists (type:String, id:String):Bool {
+		
+		var object = getObject (type, id);
+		var propertyName = getPropertyName (id);
+		
+		if (object != null && propertyName != null) {
+			
+			return Reflect.hasField (object, propertyName);
+			
+		}
+		
+		return false;
+		
+	}
+	
+	
+	public function set (type:String, id:String, value:Dynamic):Void {
+		
+		var object = getObject (type, id);
+		var propertyName = getPropertyName (id);
+		
+		Reflect.setField (object, propertyName, value);
+		
+	}
+	
+	
+}
