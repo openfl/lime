c90aaa2eddb2786022cc03c3a8ebc30e12d645fd
Joshua <joshua@jandyco.com>
2014-05-11 14:26:20 -0400

    Force load thirdparthy libraries

--- src/platforms/IOSPlatform.hx
+++ src/platforms/IOSPlatform.hx
@@ -102,6 +102,9 @@ class IOSPlatform implements IPlatformTool {
 				if (dependency.path != "") {
 					
 					var name = Path.withoutDirectory (Path.withoutExtension (dependency.path));
+
+                    project.config.ios.linkerFlags.push ("-force_load $SRCROOT/$PRODUCT_NAME/lib/$ARCHS/" + 
+                            Path.withoutDirectory (dependency.path));
 					
 					if (StringTools.startsWith (name, "lib")) {
 						
