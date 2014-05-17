b397a7c94f775bf7eddd301cb0bd1578e4ba697b
underscorediscovery <sven@underscorediscovery.com>
2014-05-16 23:17:11 -0230

    fixing iOS linker flags being an array in the template file

--- src/platforms/IOSPlatform.hx
+++ src/platforms/IOSPlatform.hx
@@ -227,7 +227,15 @@ class IOSPlatform implements IPlatformTool {
 		
 		context.IOS_COMPILER = project.config.ios.compiler;
 		context.CPP_BUILD_LIBRARY = project.config.cpp.buildLibrary;
-		context.IOS_LINKER_FLAGS = ["-stdlib=libc++"].concat (project.config.ios.linkerFlags);
+
+		var linker_flags_string = '';
+		var linker_flags_final = ["-stdlib=libc++"].concat(project.config.ios.linkerFlags);
+
+		for(_flag in linker_flags_final) {
+			linker_flags_string += '"${_flag}",';
+		}
+
+		context.IOS_LINKER_FLAGS = linker_flags_string;
 
 		switch (project.window.orientation) {
 			
