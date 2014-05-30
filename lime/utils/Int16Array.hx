9c55eb45c9407534fb635ea24ae65e36f0948882
underscorediscovery <sven@underscorediscovery.com>
2014-05-30 16:08:30 -0230

    Int16Array on html5 should have pointed to correct version. Should resolve #159

--- lime/utils/Int16Array.hx
+++ lime/utils/Int16Array.hx
@@ -1,79 +1,88 @@
 package lime.utils;
 
 
+#if lime_html5
+        
+    typedef Int16Array = js.html.Int16Array;
 
-class Int16Array extends ArrayBufferView implements ArrayAccess<Int> {
-    
-    
-    static public inline var SBYTES_PER_ELEMENT = 2;
-    
-    public var BYTES_PER_ELEMENT (default, null):Int;
-    public var length (default, null):Int;
-    
-    
-    public function new (bufferOrArray:Dynamic, start:Int = 0, length:Null<Int> = null) {
+#end //lime_html5
+
+#if lime_native
+
+    class Int16Array extends ArrayBufferView implements ArrayAccess<Int> {
         
-        BYTES_PER_ELEMENT = 2;
         
-        if (Std.is (bufferOrArray, Int)) {
+        static public inline var SBYTES_PER_ELEMENT = 2;
+        
+        public var BYTES_PER_ELEMENT (default, null):Int;
+        public var length (default, null):Int;
+        
+        
+        public function new (bufferOrArray:Dynamic, start:Int = 0, length:Null<Int> = null) {
             
-            super (Std.int (bufferOrArray) << 1);
+            BYTES_PER_ELEMENT = 2;
             
-        } else if (Std.is (bufferOrArray, Array)) {
-            
-            var ints:Array<Int> = bufferOrArray;
-            
-            if (length != null) {
+            if (Std.is (bufferOrArray, Int)) {
                 
-                this.length = length;
+                super (Std.int (bufferOrArray) << 1);
+                
+            } else if (Std.is (bufferOrArray, Array)) {
+                
+                var ints:Array<Int> = bufferOrArray;
+                
+                if (length != null) {
+                    
+                    this.length = length;
+                    
+                } else {
+                    
+                    this.length = ints.length - start;
+                    
+                }
+                
+                super (this.length << 1);
+                
+                #if !cpp
+                buffer.position = 0;
+                #end
+                
+                for (i in 0...this.length) {
+                    
+                    #if cpp
+                    untyped __global__.__hxcpp_memory_set_i16 (bytes, (i << 1), ints[i]);
+                    #else
+                    buffer.writeShort (ints[i + start]);
+                    #end
+                    
+                }
                 
             } else {
                 
-                this.length = ints.length - start;
+                super (bufferOrArray, start, length);
                 
-            }
-            
-            super (this.length << 1);
-            
-            #if !cpp
-            buffer.position = 0;
-            #end
-            
-            for (i in 0...this.length) {
+                if ((byteLength & 0x01) > 0) {
+                    
+                    throw ("Invalid array size");
+                    
+                }
                 
-                #if cpp
-                untyped __global__.__hxcpp_memory_set_i16 (bytes, (i << 1), ints[i]);
-                #else
-                buffer.writeShort (ints[i + start]);
-                #end
+                this.length = byteLength >> 1;
                 
-            }
-            
-        } else {
-            
-            super (bufferOrArray, start, length);
-            
-            if ((byteLength & 0x01) > 0) {
-                
-                throw ("Invalid array size");
-                
-            }
-            
-            this.length = byteLength >> 1;
-            
-            if ((this.length << 1) != byteLength) {
-                
-                throw "Invalid length multiple";
+                if ((this.length << 1) != byteLength) {
+                    
+                    throw "Invalid length multiple";
+                    
+                }
                 
             }
             
         }
         
+        
+        @:noCompletion @:keep inline public function __get (index:Int):Int { return getInt16 (index << 1); }
+        @:noCompletion @:keep inline public function __set (index:Int, value:Int):Void { setInt16 (index << 1, value); }
+        
+        
     }
-    
-    
-    @:noCompletion @:keep inline public function __get (index:Int):Int { return getInt16 (index << 1); }
-    @:noCompletion @:keep inline public function __set (index:Int, value:Int):Void { setInt16 (index << 1, value); }
-    
-    
-}
+
+#end //lime_native
