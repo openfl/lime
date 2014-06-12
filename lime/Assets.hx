867e1dae63bd15fa6e64097e4a06733cbc2a0ef8
Joshua Granick <jgranick@users.noreply.github.com>
2014-06-12 13:42:25 -0700

    Add initial asset support, allow canvas and DOM rendering context to be compiled cross-platform (non-op)

---
+++ lime/Assets.hx
@@ -0,0 +1,503 @@
+package lime;
+
+
+import haxe.Unserializer;
+import lime.utils.ByteArray;
+
+
+/**
+ * <p>The Assets class provides a cross-platform interface to access 
+ * embedded images, fonts, sounds and other resource files.</p>
+ * 
+ * <p>The contents are populated automatically when an application
+ * is compiled using the OpenFL command-line tools, based on the
+ * contents of the *.nmml project file.</p>
+ * 
+ * <p>For most platforms, the assets are included in the same directory
+ * or package as the application, and the paths are handled
+ * automatically. For web content, the assets are preloaded before
+ * the start of the rest of the application. You can customize the 
+ * preloader by extending the <code>NMEPreloader</code> class,
+ * and specifying a custom preloader using <window preloader="" />
+ * in the project file.</p>
+ */
+class Assets {
+	
+	
+	private static var initialized = false;
+	private static var path (default, null) = new Map <String, String> ();
+	private static var type (default, null) = new Map <String, AssetType> ();
+	
+	
+	public static function exists (id:String, type:AssetType = null):Bool {
+		
+		initialize ();
+		
+		#if (tools && !display)
+		
+		var assetType = Assets.type.get (id);
+		
+		#if pixi
+		
+		if (assetType == IMAGE) {
+			
+			return true;
+			
+		} else {
+			
+			return false;
+			
+		}
+		
+		#end
+		
+		if (assetType != null) {
+			
+			if (assetType == type || ((type == SOUND || type == MUSIC) && (assetType == MUSIC || assetType == SOUND))) {
+				
+				return true;
+				
+			}
+			
+			#if flash
+			
+			if ((assetType == BINARY || assetType == TEXT) && type == BINARY) {
+				
+				return true;
+				
+			} else if (path.exists (id)) {
+				
+				return true;
+				
+			}
+			
+			#else
+			
+			if (type == BINARY || type == null) {
+				
+				return true;
+				
+			}
+			
+			#end
+			
+		}
+		
+		return false;
+		
+		#end
+		
+		return false;
+		
+	}
+	
+	
+	/**
+	 * Gets an instance of an embedded binary asset
+	 * @usage		var bytes = Assets.getBytes("file.zip");
+	 * @param	id		The ID or asset path for the file
+	 * @return		A new ByteArray object
+	 */
+	public static function getBytes (id:String):ByteArray {
+		
+		initialize ();
+		
+		#if (tools && !display)
+		
+		#if (flash)
+		
+		//return cast (Type.createInstance (className.get (id), []), ByteArray);
+
+		#elseif (js || openfl_html5 || pixi)
+		
+		var bytes:ByteArray = null;
+		var data = ApplicationMain.urlLoaders.get (path.get (id)).data;
+		
+		if (Std.is (data, String)) {
+			
+			bytes = new ByteArray ();
+			bytes.writeUTFBytes (data);
+			
+		} else if (Std.is (data, ByteArray)) {
+			
+			bytes = cast data;
+			
+		} else {
+			
+			bytes = null;
+			
+		}
+
+		if (bytes != null) {
+			
+			bytes.position = 0;
+			return bytes;
+			
+		} else {
+			
+			return null;
+		}
+		
+		#else
+		
+		//if (className.exists(id)) return cast (Type.createInstance (className.get (id), []), ByteArray);
+		/*else*/ return ByteArray.readFile (path.get (id));
+		
+		#end
+		
+		#end
+		
+		return null;
+		
+	}
+	
+	
+	/**
+	 * Gets the file path (if available) for an asset
+	 * @usage		var path = Assets.getPath("image.jpg");
+	 * @param	id		The ID or asset path for the asset
+	 * @return		The path to the asset (or null)
+	 */
+	public static function getPath (id:String):String {
+		
+		initialize ();
+		
+		#if (tools && !display)
+		
+		#if ios
+		
+		return SystemPath.applicationDirectory + "/assets/" + path.get (id);
+		
+		#else
+		
+		return path.get (id);
+		
+		#end
+		
+		#end
+		
+		return null;
+		
+	}
+	
+	
+	/**
+	 * Gets an instance of an embedded text asset
+	 * @usage		var text = Assets.getText("text.txt");
+	 * @param	id		The ID or asset path for the file
+	 * @return		A new String object
+	 */
+	public static function getText (id:String):String {
+		
+		initialize ();
+		
+		#if (tools && !display)
+		
+		#if js
+		
+		var bytes:ByteArray = null;
+		var data = ApplicationMain.urlLoaders.get (path.get (id)).data;
+		
+		if (Std.is (data, String)) {
+			
+			return cast data;
+			
+		} else if (Std.is (data, ByteArray)) {
+			
+			bytes = cast data;
+			
+		} else {
+			
+			bytes = null;
+			
+		}
+		
+		if (bytes != null) {
+			
+			bytes.position = 0;
+			return bytes.readUTFBytes (bytes.length);
+			
+		} else {
+			
+			return null;
+		}
+		
+		#else
+		
+		var bytes = getBytes (id);
+		
+		if (bytes == null) {
+			
+			return null;
+			
+		} else {
+			
+			return bytes.readUTFBytes (bytes.length);
+			
+		}
+		
+		#end
+		
+		#end
+		
+		return null;
+		
+	}
+	
+	
+	private static function initialize ():Void {
+		
+		if (!initialized) {
+			
+			#if (tools && !display)
+			
+			loadManifest ();
+			
+			#end
+			
+			initialized = true;
+			
+		}
+		
+	}
+	
+	
+	public static function isLocal (id:String, type:AssetType = null, useCache:Bool = true):Bool {
+		
+		initialize ();
+		
+		#if (tools && !display)
+		
+		#if flash
+		
+		if (type != AssetType.MUSIC && type != AssetType.SOUND) {
+			
+			//return className.exists (id);
+			
+		}
+		
+		#end
+		
+		return true;
+		
+		#end
+		
+		return false;
+		
+	}
+	
+	
+	public static function list (type:AssetType = null):Array<String> {
+		
+		initialize ();
+		
+		var items = [];
+		
+		for (id in Assets.type.keys ()) {
+			
+			if (type == null || exists (id, type)) {
+				
+				items.push (id);
+				
+			}
+			
+		}
+		
+		return items;
+		
+	}
+	
+	
+	public static function loadBytes (id:String, handler:ByteArray -> Void):Void {
+		
+		initialize ();
+		
+		#if (tools && !display)
+		
+		#if pixi
+		
+		handler (getBytes (id));
+		
+		#elseif (flash || js)
+		
+		if (path.exists (id)) {
+			
+			var loader = new URLLoader ();
+			loader.addEventListener (Event.COMPLETE, function (event:Event) {
+				
+				var bytes = new ByteArray ();
+				bytes.writeUTFBytes (event.currentTarget.data);
+				bytes.position = 0;
+				
+				handler (bytes);
+				
+			});
+			loader.load (new URLRequest (path.get (id)));
+			
+		} else {
+			
+			handler (getBytes (id));
+			
+		}
+		
+		#else
+		
+		handler (getBytes (id));
+		
+		#end
+		
+		#end
+		
+		handler (null);
+		
+	}
+	
+	
+	private static function loadManifest ():Void {
+		
+		try {
+			
+			#if blackberry
+			var bytes = ByteArray.readFile ("app/native/manifest");
+			#elseif tizen
+			var bytes = ByteArray.readFile ("../res/manifest");
+			#elseif emscripten
+			var bytes = ByteArray.readFile ("assets/manifest");
+			#else
+			var bytes = ByteArray.readFile ("manifest");
+			#end
+			
+			if (bytes != null) {
+				
+				bytes.position = 0;
+				
+				if (bytes.length > 0) {
+					
+					var data = bytes.readUTFBytes (bytes.length);
+					
+					if (data != null && data.length > 0) {
+						
+						var manifest:Array<Dynamic> = Unserializer.run (data);
+						
+						for (asset in manifest) {
+							
+							//if (!className.exists (asset.id)) {
+								
+								path.set (asset.id, asset.path);
+								type.set (asset.id, Type.createEnum (AssetType, asset.type));
+								
+							//}
+							
+						}
+						
+					}
+					
+				}
+				
+			} else {
+				
+				trace ("Warning: Could not load asset manifest (bytes was null)");
+				
+			}
+		
+		} catch (e:Dynamic) {
+			
+			trace ('Warning: Could not load asset manifest (${e})');
+			
+		}
+		
+	}
+	
+	
+	public static function loadText (id:String, handler:String -> Void):Void {
+		
+		initialize ();
+		
+		#if (tools && !display)
+		
+		#if js
+		
+		if (path.exists (id)) {
+			
+			var loader = new URLLoader ();
+			loader.addEventListener (Event.COMPLETE, function (event:Event) {
+				
+				handler (event.currentTarget.data);
+				
+			});
+			loader.load (new URLRequest (path.get (id)));
+			
+		} else {
+			
+			handler (getText (id));
+			
+		}
+		
+		#else
+		
+		var callback = function (bytes:ByteArray):Void {
+			
+			if (bytes == null) {
+				
+				handler (null);
+				
+			} else {
+				
+				handler (bytes.readUTFBytes (bytes.length));
+				
+			}
+			
+		}
+		
+		loadBytes (id, callback);
+		
+		#end
+		
+		#end
+		
+		handler (null);
+		
+	}
+	
+	
+	private static function resolveClass (name:String):Class <Dynamic> {
+		
+		return Type.resolveClass (name);
+		
+	}
+	
+	
+	private static function resolveEnum (name:String):Enum <Dynamic> {
+		
+		var value = Type.resolveEnum (name);
+		
+		#if flash
+		
+		if (value == null) {
+			
+			return cast Type.resolveClass (name);
+			
+		}
+		
+		#end
+		
+		return value;
+		
+	}
+	
+	
+}
+
+
+enum AssetType {
+	
+	BINARY;
+	FONT;
+	IMAGE;
+	MOVIE_CLIP;
+	MUSIC;
+	SOUND;
+	TEMPLATE;
+	TEXT;
+	
+}
--- lime/graphics/canvas/CanvasRenderContext.hx
+++ lime/graphics/canvas/CanvasRenderContext.hx
@@ -4,7 +4,38 @@ typedef CanvasRenderContext = js.html.CanvasRenderingContext2D;
 #else
 
 
+typedef CanvasElement = Dynamic;
+typedef CanvasGradient = Dynamic;
+typedef CanvasPattern = Dynamic;
+typedef ImageData = Dynamic;
+typedef ImageElement = Dynamic;
+typedef TextMetrics = Dynamic;
+typedef VideoElement = Dynamic;
+
+
 class CanvasRenderContext {
+	
+	
+	public var backingStorePixelRatio (default, null):Float;
+	public var canvas:CanvasElement;
+	public var fillStyle:Dynamic;
+	public var font:String;
+	public var globalAlpha:Float;
+	public var globalCompositeOperation:String;
+	public var imageSmoothingEnabled:Bool;
+	public var lineCap:String;
+	public var lineDash:Array<Dynamic>;
+	public var lineDashOffset:Float;
+	public var lineJoin:String;
+	public var lineWidth:Float;
+	public var miterLimit:Float;
+	public var shadowBlur:Float;
+	public var shadowColor:String;
+	public var shadowOffsetX:Float;
+	public var shadowOffsetY:Float;
+	public var strokeStyle:Dynamic;
+	public var textAlign:String;
+	public var textBaseline:String;
 	
 	
 	public function new () {
@@ -12,6 +43,77 @@ class CanvasRenderContext {
 		
 		
 	}
+	
+	
+	public function arc(x:Float, y:Float, radius:Float, startAngle:Float, endAngle:Float, anticlockwise:Bool):Void {};
+	public function arcTo(x1:Float, y1:Float, x2:Float, y2:Float, radius:Float):Void {};
+	public function beginPath():Void {};
+	public function bezierCurveTo(cp1x:Float, cp1y:Float, cp2x:Float, cp2y:Float, x:Float, y:Float):Void {};
+	public function clearRect(x:Float, y:Float, width:Float, height:Float):Void {};
+	public function clearShadow():Void {};
+	public function clip():Void {};
+	public function closePath():Void {};
+	@:overload(function(imagedata:ImageData):ImageData {})
+	public function createImageData(sw:Float, sh:Float):ImageData { return null; };
+	public function createLinearGradient(x0:Float, y0:Float, x1:Float, y1:Float):CanvasGradient { return null; };
+	@:overload(function(canvas:CanvasElement, repetitionType:String):CanvasPattern {})
+	public function createPattern(image:ImageElement, repetitionType:String):CanvasPattern { return null; };
+	public function createRadialGradient(x0:Float, y0:Float, r0:Float, x1:Float, y1:Float, r1:Float):CanvasGradient { return null; };
+	@:overload(function(image:ImageElement, x:Float, y:Float):Void {})
+	@:overload(function(image:ImageElement, x:Float, y:Float, width:Float, height:Float):Void {})
+	@:overload(function(image:ImageElement, sx:Float, sy:Float, sw:Float, sh:Float, dx:Float, dy:Float, dw:Float, dh:Float):Void {})
+	@:overload(function(canvas:CanvasElement, x:Float, y:Float):Void {})
+	@:overload(function(canvas:CanvasElement, x:Float, y:Float, width:Float, height:Float):Void {})
+	@:overload(function(canvas:CanvasElement, sx:Float, sy:Float, sw:Float, sh:Float, dx:Float, dy:Float, dw:Float, dh:Float):Void {})
+	@:overload(function(video:VideoElement, x:Float, y:Float):Void {})
+	@:overload(function(video:VideoElement, x:Float, y:Float, width:Float, height:Float):Void {})
+	public function drawImage(video:VideoElement, sx:Float, sy:Float, sw:Float, sh:Float, dx:Float, dy:Float, dw:Float, dh:Float):Void {};
+	public function drawImageFromRect(image:ImageElement, ?sx:Float, ?sy:Float, ?sw:Float, ?sh:Float, ?dx:Float, ?dy:Float, ?dw:Float, ?dh:Float, ?compositeOperation:String):Void {};
+	public function fill():Void {};
+	public function fillRect(x:Float, y:Float, width:Float, height:Float):Void {};
+	public function fillText(text:String, x:Float, y:Float, ?maxWidth:Float):Void {};
+	public function getImageData(sx:Float, sy:Float, sw:Float, sh:Float):ImageData { return null; };
+	public function getImageDataHD(sx:Float, sy:Float, sw:Float, sh:Float):ImageData { return null; };
+	public function getLineDash():Array<Float> { return null; };
+	public function isPointInPath(x:Float, y:Float):Bool { return false; };
+	public function lineTo(x:Float, y:Float):Void {};
+	public function measureText(text:String):TextMetrics { return null; };
+	public function moveTo(x:Float, y:Float):Void {};
+	@:overload(function(imagedata:ImageData, dx:Float, dy:Float):Void {})
+	public function putImageData(imagedata:ImageData, dx:Float, dy:Float, dirtyX:Float, dirtyY:Float, dirtyWidth:Float, dirtyHeight:Float):Void {};
+	@:overload(function(imagedata:ImageData, dx:Float, dy:Float):Void {})
+	public function putImageDataHD(imagedata:ImageData, dx:Float, dy:Float, dirtyX:Float, dirtyY:Float, dirtyWidth:Float, dirtyHeight:Float):Void {};
+	public function quadraticCurveTo(cpx:Float, cpy:Float, x:Float, y:Float):Void {};
+	public function rect(x:Float, y:Float, width:Float, height:Float):Void {};
+	public function restore():Void {};
+	public function rotate(angle:Float):Void {};
+	public function save():Void {};
+	public function scale(sx:Float, sy:Float):Void {};
+	public function setAlpha(alpha:Float):Void {};
+	public function setCompositeOperation(compositeOperation:String):Void {};
+	@:overload(function(color:String, ?alpha:Float):Void {})
+	@:overload(function(grayLevel:Float, ?alpha:Float):Void {})
+	@:overload(function(r:Float, g:Float, b:Float, a:Float):Void {})
+	public function setFillColor(c:Float, m:Float, y:Float, k:Float, a:Float):Void {};
+	public function setLineCap(cap:String):Void {};
+	public function setLineDash(dash:Array<Float>):Void {};
+	public function setLineJoin(join:String):Void {};
+	public function setLineWidth(width:Float):Void {};
+	public function setMiterLimit(limit:Float):Void {};
+	@:overload(function(width:Float, height:Float, blur:Float, ?color:String, ?alpha:Float):Void {})
+	@:overload(function(width:Float, height:Float, blur:Float, grayLevel:Float, ?alpha:Float):Void {})
+	@:overload(function(width:Float, height:Float, blur:Float, r:Float, g:Float, b:Float, a:Float):Void {})
+	public function setShadow(width:Float, height:Float, blur:Float, c:Float, m:Float, y:Float, k:Float, a:Float):Void {};
+	@:overload(function(color:String, ?alpha:Float):Void {})
+	@:overload(function(grayLevel:Float, ?alpha:Float):Void {})
+	@:overload(function(r:Float, g:Float, b:Float, a:Float):Void {})
+	public function setStrokeColor (c:Float, m:Float, y:Float, k:Float, a:Float):Void {};
+	public function setTransform (m11:Float, m12:Float, m21:Float, m22:Float, dx:Float, dy:Float):Void {};
+	public function stroke ():Void {};
+	public function strokeRect (x:Float, y:Float, width:Float, height:Float, ?lineWidth:Float):Void {};
+	public function strokeText (text:String, x:Float, y:Float, ?maxWidth:Float):Void {};
+	public function transform (m11:Float, m12:Float, m21:Float, m22:Float, dx:Float, dy:Float):Void {};
+	public function translate (tx:Float, ty:Float):Void {};
 	
 	
 }
