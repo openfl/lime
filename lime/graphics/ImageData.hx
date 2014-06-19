06bfc9e7e7f6dc6d059de07d943ccc47430ac64e
Joshua Granick <jgranick@users.noreply.github.com>
2014-06-19 01:04:27 -0700

    Compile fix

--- lime/graphics/ImageData.hx
+++ lime/graphics/ImageData.hx
@@ -1,23 +1,31 @@
 package lime.graphics;
 
 
-import lime.utils.UInt8Array;
-
-
 class ImageData {
 	
 	
-	public var data:UInt8Array;
+	public var data:ImageDataType;
 	public var height:Int;
 	public var width:Int;
 	
 	
-	public function new () {
+	public function new (data:ImageDataType = null, width:Int = 0, height:Int = 0) {
 		
-		
+		this.data = data;
+		this.width = width;
+		this.height = height;
 		
 	}
 	
 	
 	
 }
+
+
+#if js
+typedef ImageDataType = js.html.Image;
+#elseif flash
+typedef ImageDataType = flash.display.BitmapData;
+#else
+typedef ImageDataType = lime.utils.UInt8Array;
+#end
