7c4cb01a260e779800a0f4b4c7bc7b9e532e23d6
Joshua Granick <jgranick@users.noreply.github.com>
2014-06-26 10:36:52 -0700

    Move 'triggerFrame' logic to exec()

--- lime/app/Application.hx
+++ lime/app/Application.hx
@@ -54,43 +54,6 @@ class Application extends Module {
 		windows.push (window);
 		window.create (this);
 		
-		#if js
-		
-		untyped __js__ ("
-			var lastTime = 0;
-			var vendors = ['ms', 'moz', 'webkit', 'o'];
-			for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-				window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-				window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
-										   || window[vendors[x]+'CancelRequestAnimationFrame'];
-			}
-			
-			if (!window.requestAnimationFrame)
-				window.requestAnimationFrame = function(callback, element) {
-					var currTime = new Date().getTime();
-					var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-					var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
-					  timeToCall);
-					lastTime = currTime + timeToCall;
-					return id;
-				};
-			
-			if (!window.cancelAnimationFrame)
-				window.cancelAnimationFrame = function(id) {
-					clearTimeout(id);
-				};
-			
-			window.requestAnimFrame = window.requestAnimationFrame;
-		");
-		
-		__triggerFrame ();
-		
-		#elseif flash
-		
-		Lib.current.stage.addEventListener (flash.events.Event.ENTER_FRAME, __triggerFrame);
-		
-		#end
-		
 	}
 	
 	
@@ -139,10 +102,47 @@ class Application extends Module {
 	public function exec ():Int {
 		
 		#if (cpp || neko)
+		
 		return lime_application_exec (__handle);
-		#else
-		return 0;
+		
+		#elseif js
+		
+		untyped __js__ ("
+			var lastTime = 0;
+			var vendors = ['ms', 'moz', 'webkit', 'o'];
+			for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+				window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+				window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+										   || window[vendors[x]+'CancelRequestAnimationFrame'];
+			}
+			
+			if (!window.requestAnimationFrame)
+				window.requestAnimationFrame = function(callback, element) {
+					var currTime = new Date().getTime();
+					var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+					var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+					  timeToCall);
+					lastTime = currTime + timeToCall;
+					return id;
+				};
+			
+			if (!window.cancelAnimationFrame)
+				window.cancelAnimationFrame = function(id) {
+					clearTimeout(id);
+				};
+			
+			window.requestAnimFrame = window.requestAnimationFrame;
+		");
+		
+		__triggerFrame ();
+		
+		#elseif flash
+		
+		Lib.current.stage.addEventListener (flash.events.Event.ENTER_FRAME, __triggerFrame);
+		
 		#end
+		
+		return 0;
 		
 	}
 	
