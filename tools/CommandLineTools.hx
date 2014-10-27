8e014d6a5751d20472e0c111e32a6667cfb7bbea
Joshua Granick <jgranick@users.noreply.github.com>
2014-10-27 10:04:46 -0700

    Better OpenFL ASCII color on Mac

--- tools/CommandLineTools.hx
+++ tools/CommandLineTools.hx
@@ -760,16 +760,16 @@ class CommandLineTools {
 		
 		if (targetFlags.exists ("openfl")) {
 			
-			LogHelper.println (" .d88 88b.                             \x1b[0m\x1b[1;36m888888b 888 \x1b[0m");
-			LogHelper.println ("d88P\" \"Y88b                            \x1b[0m\x1b[1;36m888     888 \x1b[0m");
-			LogHelper.println ("888     888                            \x1b[0m\x1b[1;36m888     888 \x1b[0m");
-			LogHelper.println ("888     888 88888b.   .d88b.  88888b.  \x1b[0m\x1b[1;36m8888888 888 \x1b[0m");
-			LogHelper.println ("888     888 888 \"88b d8P  Y8b 888 \"88b \x1b[0m\x1b[1;36m888     888 \x1b[0m");
-			LogHelper.println ("888     888 888  888 88888888 888  888 \x1b[0m\x1b[1;36m888     888 \x1b[0m");
-			LogHelper.println ("Y88b. .d88P 888 d88P Y8b.     888  888 \x1b[0m\x1b[1;36m888     888 \x1b[0m");
-			LogHelper.println (" \"Y88 88P\"  88888P\"   \"Y8888  888  888 \x1b[0m\x1b[1;36m888     \"Y888P \x1b[0m");
-			LogHelper.println ("            888                                   ");
-			LogHelper.println ("            888                                   \x1b[0m");
+			LogHelper.println ("\x1b[37m .d88 88b.                             \x1b[0m\x1b[1;36m888888b 888 \x1b[0m");
+			LogHelper.println ("\x1b[37md88P\" \"Y88b                            \x1b[0m\x1b[1;36m888     888 \x1b[0m");
+			LogHelper.println ("\x1b[37m888     888                            \x1b[0m\x1b[1;36m888     888 \x1b[0m");
+			LogHelper.println ("\x1b[37m888     888 88888b.   .d88b.  88888b.  \x1b[0m\x1b[1;36m8888888 888 \x1b[0m");
+			LogHelper.println ("\x1b[37m888     888 888 \"88b d8P  Y8b 888 \"88b \x1b[0m\x1b[1;36m888     888 \x1b[0m");
+			LogHelper.println ("\x1b[37m888     888 888  888 88888888 888  888 \x1b[0m\x1b[1;36m888     888 \x1b[0m");
+			LogHelper.println ("\x1b[37mY88b. .d88P 888 d88P Y8b.     888  888 \x1b[0m\x1b[1;36m888     888 \x1b[0m");
+			LogHelper.println ("\x1b[37m \"Y88 88P\"  88888P\"   \"Y8888  888  888 \x1b[0m\x1b[1;36m888     \"Y888P \x1b[0m");
+			LogHelper.println ("\x1b[37m            888                                   ");
+			LogHelper.println ("\x1b[37m            888                                   \x1b[0m");
 			
 			LogHelper.println ("");
 			LogHelper.println ("\x1b[1mOpenFL Command-Line Tools\x1b[0;1m (" + getVersion (new Haxelib ("openfl")) + "-L" + StringHelper.generateUUID (5, null, StringHelper.generateHashCode (version)) + ")\x1b[0m");
