3f548d2f4b93ecdc23300c755381bfa99648a43d
Joshua Granick <jgranick@users.noreply.github.com>
2015-02-19 14:00:29 -0800

    Try using system 'zip' command in Mac/Linux ZipHelper

--- lime/tools/helpers/ZipHelper.hx
+++ lime/tools/helpers/ZipHelper.hx
@@ -32,33 +32,42 @@ class ZipHelper {
 		}
 		
 		PathHelper.mkdir (Path.directory (targetPath));
-		var output = File.write (targetPath, true);
 		
-		/*if (Path.extension (path) == "crx") {
+		if (PlatformHelper.hostPlatform == WINDOWS) {
 			
-			var input = File.read (defines.get ("KEY_STORE"), true);
-			var publicKey:Bytes = input.readAll ();
-			input.close ();
+			var output = File.write (targetPath, true);
 			
-			var signature = SHA1.encode ("this isn't working");
+			/*if (Path.extension (path) == "crx") {
+				
+				var input = File.read (defines.get ("KEY_STORE"), true);
+				var publicKey:Bytes = input.readAll ();
+				input.close ();
+				
+				var signature = SHA1.encode ("this isn't working");
+				
+				output.writeString ("Cr24"); // magic number
+				output.writeInt32 (Int32.ofInt (2)); // CRX file format version
+				output.writeInt32 (Int32.ofInt (publicKey.length)); // public key length
+				output.writeInt32 (Int32.ofInt (signature.length)); // signature length
+				output.writeBytes (publicKey, 0, publicKey.length);
+				output.writeString (signature);
+				
+				//output.writeBytes (); // public key contents "The contents of the author's RSA public key, formatted as an X509 SubjectPublicKeyInfo block. "
+				//output.writeBytes (); // "The signature of the ZIP content using the author's private key. The signature is created using the RSA algorithm with the SHA-1 hash function."
+				
+			}*/
 			
-			output.writeString ("Cr24"); // magic number
-			output.writeInt32 (Int32.ofInt (2)); // CRX file format version
-			output.writeInt32 (Int32.ofInt (publicKey.length)); // public key length
-			output.writeInt32 (Int32.ofInt (signature.length)); // signature length
-			output.writeBytes (publicKey, 0, publicKey.length);
-			output.writeString (signature);
+			LogHelper.info ("", " - \x1b[1mWriting file:\x1b[0m " + targetPath);
 			
-			//output.writeBytes (); // public key contents "The contents of the author's RSA public key, formatted as an X509 SubjectPublicKeyInfo block. "
-			//output.writeBytes (); // "The signature of the ZIP content using the author's private key. The signature is created using the RSA algorithm with the SHA-1 hash function."
+			var writer = new Writer (output);
+			writer.write (cast files);
+			output.close ();
 			
-		}*/
-		
-		LogHelper.info ("", " - \x1b[1mWriting file:\x1b[0m " + targetPath);
-		
-		var writer = new Writer (output);
-		writer.write (cast files);
-		output.close ();
+		} else {
+			
+			ProcessHelper.runCommand (Path.directory (targetPath), "zip", [ "-r", targetPath, path ]);
+			
+		}
 		
 	}
 	
