8ed6ad3d371cd052676fa92edbd4992b3b3d614c
vroad <xvroad@gmail.com>
2015-03-04 17:48:11 +0900

    - Use "iojs" instead in NodeJSHelper (since io.js doesn't work correctly with "node")  - Remove nodejs executables from repo

--- lime/tools/helpers/NodeJSHelper.hx
+++ lime/tools/helpers/NodeJSHelper.hx
@@ -12,39 +12,7 @@ class NodeJSHelper {
 	
 	
 	public static function run (project:HXProject, modulePath:String, args:Array<String> = null):Void {
-		
-		var suffix = switch (PlatformHelper.hostPlatform) {
-			
-			case Platform.WINDOWS: "-windows.exe";
-			case Platform.MAC: "-mac";
-			case Platform.LINUX: "-linux";
-			default: return;
-			
-		}
-		
-		if (suffix == "-linux") {
-			
-			if (PlatformHelper.hostArchitecture == Architecture.X86) {
-				
-				suffix += "32";
-				
-			} else {
-				
-				suffix += "64";
-				
-			}
-			
-		}
-		
-		var templatePaths = [ PathHelper.combine (PathHelper.getHaxelib (new Haxelib ("lime")), "templates") ].concat (project.templatePaths);
-		var node = PathHelper.findTemplate (templatePaths, "bin/node/node" + suffix);
-		
-		if (PlatformHelper.hostPlatform != Platform.WINDOWS) {
-			
-			Sys.command ("chmod", [ "+x", node ]);
-			
-		}
-		
+
 		if (args == null) {
 			
 			args = [];
@@ -53,7 +21,7 @@ class NodeJSHelper {
 		
 		args.unshift (Path.withoutDirectory (modulePath));
 		
-		ProcessHelper.runCommand (Path.directory (modulePath), node, args);
+		ProcessHelper.runCommand (Path.directory (modulePath), "iojs", args);
 		
 	}
 	
