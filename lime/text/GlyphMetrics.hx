b460dfc93c7558a33e71b5fed8a38e81d6419278
Joshua Granick <jgranick@users.noreply.github.com>
2015-03-09 08:26:23 -0700

    Fill in GlyphMetrics type

--- lime/text/GlyphMetrics.hx
+++ lime/text/GlyphMetrics.hx
@@ -1,15 +1,15 @@
 package lime.text;
 
-/**
- * ...
- * @author ...
- */
-class GlyphMetrics
-{
 
-	public function new() 
-	{
-		
-	}
+typedef GlyphMetrics = {
+	
+	var height:Int;
+	var horizontalAdvance:Int;
+	var horizontalBearingX:Int;
+	var horizontalBearingY:Int;
+	var index:Int;
+	var verticalAdvance:Int;
+	var verticalBearingX:Int;
+	var verticalBearingY:Int;
 	
 }
