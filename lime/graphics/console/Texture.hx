5f7157ef927d46f999d13e5c03e7378f03b24c9b
James Gray <james@james4k.com>
2015-04-01 01:45:46 -0500

    console: updateable textures

--- lime/graphics/console/Texture.hx
+++ lime/graphics/console/Texture.hx
@@ -1,6 +1,8 @@
 package lime.graphics.console; #if lime_console
 
 
+import cpp.UInt8;
+import cpp.Pointer;
 import lime.ConsoleIncludePaths;
 
 
@@ -10,6 +12,9 @@ extern class Texture {
 	
 	// valid returns true if this represents a non-zero handle to a texture.
 	public var valid (get, never):Bool;
+
+	public function update (data:TextureData):Void;
+	public function updateFromRGBA (data:Pointer<UInt8>):Void;
 
 	private function get_valid ():Bool;
 
