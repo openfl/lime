48a888d2dda7aca6f6938f549bfb17dd04ff0adf
Joshua Granick <jgranick@users.noreply.github.com>
2015-04-04 23:04:24 -0700

    Use 'lime_hybrid' instead of 'hybrid'

--- lime/utils/ByteArray.hx
+++ lime/utils/ByteArray.hx
@@ -109,7 +109,7 @@ class ByteArray #if !js extends Bytes implements ArrayAccess<Int> implements IDa
 		var init = System.load ("lime", "lime_byte_array_init", 4);
 		init (factory, slen, resize, bytes);
 		
-		#if hybrid
+		#if lime_hybrid
 		var init = System.load ("lime-legacy", "lime_byte_array_init", 4);
 		init (factory, slen, resize, bytes);
 		#end
