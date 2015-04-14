4d135c8f03c1d13a3c7e3b99dcde63ac960e6254
Joshua Granick <jgranick@users.noreply.github.com>
2015-04-14 16:28:27 -0700

    Fix default 'disableCFFI' value

--- lime/system/System.hx
+++ lime/system/System.hx
@@ -29,7 +29,7 @@ class System {
 	public static var applicationDirectory (get, null):String;
 	public static var applicationStorageDirectory (get, null):String;
 	public static var desktopDirectory (get, null):String;
-	public static var disableCFFI:Bool = #if (disable_cffi || macro || !cpp || !neko || !nodejs) true #else false #end;
+	public static var disableCFFI:Bool = #if ((cpp || neko || nodejs) && !(disable_cffi || macro)) false #else true #end;
 	public static var documentsDirectory (get, null):String;
 	public static var fontsDirectory (get, null):String;
 	public static var userDirectory (get, null):String;
