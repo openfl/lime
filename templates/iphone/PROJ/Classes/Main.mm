5cb840381e2622da5b43c7a394cef96c6498fd1c
Joshua Granick <jgranick@users.noreply.github.com>
2015-05-30 18:04:47 -0700

    iOS fix

--- templates/iphone/PROJ/Classes/Main.mm
+++ templates/iphone/PROJ/Classes/Main.mm
@@ -1,19 +1,13 @@
-/*
- *  Main.mm
- *
- *  Boot code for lime.
- *
- */
-
 #include <stdio.h>
 
 extern "C" const char *hxRunLibrary ();
 extern "C" void hxcpp_set_top_of_stack ();
 
 extern "C" int zlib_register_prims ();
+extern "C" int lime_cairo_register_prims ();
 extern "C" int lime_openal_register_prims ();
-::foreach ndlls::::if (registerStatics)::extern "C" int ::nameSafe::_register_prims ();::end::
-::end::
+::foreach ndlls::::if (registerStatics)::
+extern "C" int ::nameSafe::_register_prims ();::end::::end::
 
 
 extern "C" int SDL_main (int argc, char *argv[]) {
@@ -21,16 +15,17 @@ extern "C" int SDL_main (int argc, char *argv[]) {
 	hxcpp_set_top_of_stack ();
 	
 	zlib_register_prims ();
+	lime_cairo_register_prims ();
 	lime_openal_register_prims ();
-	::foreach ndlls::::if (registerStatics)::::nameSafe::_register_prims ();::end::
-	::end::
+	::foreach ndlls::::if (registerStatics)::
+	::nameSafe::_register_prims ();::end::::end::
 	
 	const char *err = NULL;
 	err = hxRunLibrary ();
 	
 	if (err) {
 		
-		printf (" Error %s\n", err);
+		printf ("Error %s\n", err);
 		return -1;
 		
 	}
