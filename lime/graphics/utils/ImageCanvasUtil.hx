4378412a5475ae85db648765275755b087bbcc84
Joshua Granick <jgranick@users.noreply.github.com>
2015-07-22 19:23:54 -0700

    Update ImageCanvasUtil.hx

--- lime/graphics/utils/ImageCanvasUtil.hx
+++ lime/graphics/utils/ImageCanvasUtil.hx
@@ -88,12 +88,12 @@ class ImageCanvasUtil {
 	
 	public static function copyPixels (image:Image, sourceImage:Image, sourceRect:Rectangle, destPoint:Vector2, alphaImage:Image = null, alphaPoint:Vector2 = null, mergeAlpha:Bool = false):Void {
 		
-		if (destPoint.x >= sourceRect.width || destPoint.y >= sourceRect.height) {
-
+		if (destPoint.x >= image.width || destPoint.y >= image.height) {
+			
 			return;
-
+			
 		}
-
+		
 		if (alphaImage != null && alphaImage.transparent) {
 			
 			if (alphaPoint == null) alphaPoint = new Vector2 ();
