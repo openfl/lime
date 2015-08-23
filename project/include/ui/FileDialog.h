76341325ec6a63c50ff9e2b7371559a70688a3e5
Joshua Granick <jgranick@users.noreply.github.com>
2015-08-22 23:39:06 -0700

    Compile fix

--- project/include/ui/FileDialog.h
+++ project/include/ui/FileDialog.h
@@ -12,9 +12,9 @@ namespace lime {
 		
 		public:
 			
-			static const char* OpenFile (const char* filter = NULL, const char* defaultPath = NULL);
-			static void OpenFiles (std::vector<const char*>* files, const char* filter = NULL, const char* defaultPath = NULL);
-			static const char* SaveFile (const char* filter = NULL, const char* defaultPath = NULL);
+			static const char* OpenFile (const char* filter = 0, const char* defaultPath = 0);
+			static void OpenFiles (std::vector<const char*>* files, const char* filter = 0, const char* defaultPath = 0);
+			static const char* SaveFile (const char* filter = 0, const char* defaultPath = 0);
 		
 	};
 	
