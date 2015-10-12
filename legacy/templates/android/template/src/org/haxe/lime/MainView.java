68ede0574a85e228ccb041fc8c0970807ca1e380
Joshua Granick <jgranick@users.noreply.github.com>
2015-10-12 11:34:17 -0700

    Resolve no text input on Galaxy S6 predictive text keyboard issue

--- legacy/templates/android/template/src/org/haxe/lime/MainView.java
+++ legacy/templates/android/template/src/org/haxe/lime/MainView.java
@@ -293,6 +293,7 @@ class MainView extends GLSurfaceView {
 			
 		};
 		
+		outAttrs.inputType = InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD;
 		outAttrs.imeOptions = EditorInfo.IME_FLAG_NO_EXTRACT_UI | 33554432 /* API 11: EditorInfo.IME_FLAG_NO_FULLSCREEN */;
 		
 		return inputConnection;
