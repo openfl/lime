83e0bd46c283a5ce2fe0c4443664bf58cfcf408c
Joshua Granick <jgranick@users.noreply.github.com>
2015-10-19 19:15:40 -0700

    Improve consistency of frame timing (thanks @mauvecow)

--- project/src/backend/sdl/SDLApplication.cpp
+++ project/src/backend/sdl/SDLApplication.cpp
@@ -721,7 +721,7 @@ namespace lime {
 		
 		#if (!defined (IPHONE) && !defined (EMSCRIPTEN))
 		
-		if (active && (firstTime || SDL_WaitEvent (&event))) {
+		if (active && (firstTime || WaitEvent (&event))) {
 			
 			firstTime = false;
 			
@@ -792,6 +792,25 @@ namespace lime {
 	void SDLApplication::UpdateFrame (void*) {
 		
 		UpdateFrame ();
+		
+	}
+	
+	
+	int SDLApplication::WaitEvent (SDL_Event *event) {
+		
+		for(;;) {
+			
+			SDL_PumpEvents ();
+			
+			switch (SDL_PeepEvents (event, 1, SDL_GETEVENT, SDL_FIRSTEVENT, SDL_LASTEVENT)) {
+				
+				case -1: return 0;
+				case 1: return 1;
+				default: SDL_Delay (1);
+				
+			}
+			
+		}
 		
 	}
 	
