13bff4cda6080753091f9c60a013815eab2196ff
Lars A. Doucet <lars.doucet@gmail.com>
2015-11-23 12:04:46 -0600

    CFFI: add code to switch between "lime" and "lime-angle" depending on what values the user sets. TODO: do this check earlier somehow to avoid this code injection and make it cleaner

--- lime/system/CFFI.hx
+++ lime/system/CFFI.hx
@@ -44,6 +44,11 @@ class CFFI {
 	 */
 	public static function load (library:String, method:String, args:Int = 0, lazy:Bool = false):Dynamic {
 		
+		if (library == "lime" || library == null)
+		{
+			library = (Sys.args().indexOf("--angle") > -1) ? "lime-angle" : "lime" ;
+		}
+		
 		#if (disable_cffi || macro)
 		var enabled = false;
 		#end
@@ -394,7 +399,11 @@ using haxe.macro.TypeTools;
 class CFFI {
 	
 	
-	public static function build (defaultLibrary:String = "lime"):Array<Field> {
+	public static function build (defaultLibrary:String = null):Array<Field> {
+		
+		if (defaultLibrary == null || defaultLibrary == "lime") {
+			defaultLibrary = '" + ((Sys.args().indexOf("--angle") > -1) ? "lime-angle" : "lime") + "';
+		}
 		
 		var pos = Context.currentPos ();
 		var fields = Context.getBuildFields ();
