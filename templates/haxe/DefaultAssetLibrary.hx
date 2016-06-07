668f59ae1cf2228e3989fc85b727ca3a0f4a1484
Valentin Lemière <valentin@ibilon.com>
2016-06-07 15:52:39 +0200

    Use application relative asset path on desktop only, fix android asset loading

--- templates/haxe/DefaultAssetLibrary.hx
+++ templates/haxe/DefaultAssetLibrary.hx
@@ -45,7 +45,7 @@ class DefaultAssetLibrary extends AssetLibrary {
 	private var lastModified:Float;
 	private var timer:Timer;
 	
-	#if sys
+	#if desktop
 	private var rootPath = FileSystem.absolutePath (Path.directory (#if (haxe_ver >= 3.3) Sys.programPath () #else Sys.executablePath () #end)) + "/";
 	#else
 	private var rootPath = "";
