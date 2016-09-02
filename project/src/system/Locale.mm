ad4c61cc35c640ceb52ced8dc9311508e4dee350
Joshua Granick <jgranick@users.noreply.github.com>
2016-09-02 15:09:06 -0700

    Implement Locale for iOS/macOS

---
+++ project/src/system/Locale.mm
@@ -0,0 +1,42 @@
+#ifdef HX_MACOS
+#import <Cocoa/Cocoa.h>
+#else
+#import <UIKit/UIKit.h>
+#endif
+
+#include <system/Locale.h>
+
+
+namespace lime {
+	
+	
+	char* Locale::GetSystemLocale () {
+		
+		#ifndef OBJC_ARC
+		NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+		#endif
+		
+		NSString* locale = [[NSLocale currentLocale] localeIdentifier];
+		char* result = 0;
+		
+		if (locale) {
+			
+			const char* ptr = [locale UTF8String];
+			result = (char*)malloc ([locale length] + 1);
+			strncpy (result, ptr, [locale length]);
+			
+			printf("%s\n", result);
+			
+		}
+		
+		#ifndef OBJC_ARC
+		[locale release];
+		[pool drain];
+		#endif
+		
+		return result;
+		
+	}
+	
+	
+}
