b6a895b7af547f1146d437c05309171bd39275d5
Joshua Granick <jgranick@users.noreply.github.com>
2016-10-17 11:31:21 -0700

    Use host Neko install

--- lime/tools/helpers/NekoHelper.hx
+++ lime/tools/helpers/NekoHelper.hx
@@ -1,10 +1,12 @@
 package lime.tools.helpers;
 
 
-import lime.project.Haxelib;
-import sys.io.File;
+import haxe.io.Path;
 import lime.tools.helpers.PlatformHelper;
+import lime.project.Haxelib;
 import lime.project.Platform;
+import sys.FileSystem;
+import sys.io.File;
 
 
 class NekoHelper {
@@ -12,14 +14,14 @@ class NekoHelper {
 	
 	public static function copyLibraries (templatePaths:Array <String>, platformName:String, targetPath:String) {
 		
-		FileHelper.recursiveCopyTemplate (templatePaths, "neko/ndll/" + platformName, targetPath);
+		//FileHelper.recursiveCopyTemplate (templatePaths, "neko/ndll/" + platformName, targetPath);
 		
 	}
 	
 	
 	public static function createExecutable (templatePaths:Array <String>, platformName:String, source:String, target:String, iconPath:String = null):Void {
 		
-		var executablePath = PathHelper.findTemplate (templatePaths, "neko/bin/neko-" + platformName);
+		/*var executablePath = PathHelper.findTemplate (templatePaths, "neko/bin/neko-" + platformName);
 		var executable = File.getBytes (executablePath);
 		var sourceContents = File.getBytes (source);
 		
@@ -28,14 +30,26 @@ class NekoHelper {
 		output.write (sourceContents);
 		output.writeString ("NEKO");
 		output.writeInt32 (executable.length);
-		output.close ();
+		output.close ();*/
+		
+		ProcessHelper.runCommand ("", "nekotools", [ "boot", PathHelper.tryFullPath (source) ]);
+		
+		var path = Path.withoutExtension (source);
+		
+		if (PlatformHelper.hostPlatform == WINDOWS) {
+			
+			path += ".exe";
+			
+		}
+		
+		FileHelper.copyFile (path, target);
 		
 	}
 	
 	
 	public static function createWindowsExecutable (templatePaths:Array <String>, source:String, target:String, iconPath:String):Void {
 		
-		var executablePath = PathHelper.findTemplate (templatePaths, "neko/bin/neko-windows");
+		/*var executablePath = PathHelper.findTemplate (templatePaths, "neko/bin/neko-windows");
 		var executable = File.getBytes (executablePath);
 		var sourceContents = File.getBytes (source);
 		
@@ -56,7 +70,9 @@ class NekoHelper {
 		output.write (sourceContents);
 		output.writeString ("NEKO");
 		output.writeInt32 (executable.length);
-		output.close ();
+		output.close ();*/
+		
+		createExecutable (templatePaths, null, source, target, iconPath);
 		
 	}
 	
