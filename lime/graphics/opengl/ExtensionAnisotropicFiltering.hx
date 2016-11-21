89d3f850286be9c2b1813f38d8319f20acaa1d7b
Greg Caldwell <caldwellgp@googlemail.com>
2016-11-21 22:44:02 +0000

    Add support for anisotropic filtering through GL extension

---
+++ lime/graphics/opengl/ExtensionAnisotropicFiltering.hx
@@ -0,0 +1,9 @@
+package lime.graphics.opengl;
+
+class ExtensionAnisotropicFiltering {
+	
+	public static var TEXTURE_MAX_ANISOTROPY_EXT = 0x84FE;
+	public static var MAX_TEXTURE_MAX_ANISOTROPY_EXT = 0x84FF;
+	
+	
+}
