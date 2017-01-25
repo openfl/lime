ebd35371e9eaa5a3a586161b336073cd17eb8eef
Joshua Granick <jgranick@users.noreply.github.com>
2017-01-25 13:43:28 -0800

    Reduce JS output size

--- lime/graphics/RenderContext.hx
+++ lime/graphics/RenderContext.hx
@@ -12,10 +12,10 @@ enum RenderContext {
 	
 	OPENGL (gl:#if (!flash || display) GLRenderContext #else Dynamic #end);
 	CANVAS (context:CanvasRenderContext);
-	DOM (element:DOMRenderContext);
-	FLASH (stage:FlashRenderContext);
-	CAIRO (cairo:CairoRenderContext);
-	CONSOLE (context:ConsoleRenderContext);
+	DOM (element:#if ((!js && !html5 && !dom) || display) DOMRenderContext #else Dynamic #end);
+	FLASH (stage:#if ((!js && !html5) || display) FlashRenderContext #else Dynamic #end);
+	CAIRO (cairo:#if ((!js && !html5) || display) CairoRenderContext #else Dynamic #end);
+	CONSOLE (context:#if ((!js && !html5) || display) ConsoleRenderContext #else Dynamic #end);
 	CUSTOM (data:Dynamic);
 	NONE;
 	
