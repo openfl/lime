062d79afd6c95a8bb5d216c42fc4040aeee250f2
RblSb <RblSb@nm.ru>
2017-05-15 00:38:03 +0300

    initial

---
+++ lime/text/unifill/CodePointIter.hx
@@ -0,0 +1,26 @@
+package lime.text.unifill;
+
+class CodePointIter {
+
+	var string : String;
+	var index : Int;
+	var endIndex : Int;
+
+	public inline function new(s : String) {
+		string = s;
+		index = 0;
+		endIndex = s.length;
+	}
+
+	public inline function hasNext() : Bool {
+		return index < endIndex;
+	}
+
+	var i = 0; // FIXME: blocked by HaxeFoundation/haxe#4353
+	public inline function next() : CodePoint {
+		i = index;
+		index += InternalEncoding.codePointWidthAt(string, index);
+		return cast InternalEncoding.codePointAt(string, i);
+	}
+
+}
