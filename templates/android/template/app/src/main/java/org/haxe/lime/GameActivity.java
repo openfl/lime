5f34bf2577046bf7024a86b449dd0014467908cd
aW4KeNiNG <DMJ5AdVZaPxTCbsF11Yu>
2017-07-01 18:33:21 +0200

    Request Permission result for Android.

--- templates/android/template/app/src/main/java/org/haxe/lime/GameActivity.java
+++ templates/android/template/app/src/main/java/org/haxe/lime/GameActivity.java
@@ -1,9 +1,9 @@
 package org.haxe.lime;
 
 
-import android.content.res.AssetManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -11,16 +11,16 @@ import android.os.Handler;
 import android.os.Vibrator;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.MimeTypeMap;
+import org.haxe.extension.Extension;
+import org.libsdl.app.SDLActivity;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.haxe.extension.Extension;
-import org.libsdl.app.SDLActivity;
 
 
 public class GameActivity extends SDLActivity {
@@ -71,6 +71,23 @@ public class GameActivity extends SDLActivity {
 		
 		super.onActivityResult (requestCode, resultCode, data);
 		
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+		for (Extension extension : extensions) {
+
+			if (!extension.onRequestPermissionsResult (requestCode, permissions, grantResults)) {
+
+				return;
+
+			}
+
+		}
+
+		super.onRequestPermissionsResult (requestCode, permissions, grantResults);
+
 	}
 	
 	
