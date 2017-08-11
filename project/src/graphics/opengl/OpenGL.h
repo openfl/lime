fc187b2b23c2e7b11e6246addb29f22f27bdea21
Joshua Granick <jgranick@users.noreply.github.com>
2017-08-10 17:22:30 -0700

    Use GLES3 headers

--- project/src/graphics/opengl/OpenGL.h
+++ project/src/graphics/opengl/OpenGL.h
@@ -5,14 +5,14 @@
 #if defined (BLACKBERRY) || defined (ANDROID) || defined (WEBOS) || defined (GPH) || defined (EMSCRIPTEN) || defined (RASPBERRYPI)
 
 #define LIME_GLES
-#include <GLES2/gl2.h>
-#include <GLES2/gl2ext.h>
+#include <GLES3/gl3.h>
+#include <GLES3/gl2ext.h>
 
 #elif defined (TIZEN)
 
 #define LIME_GLES
-#include <gl2.h>
-#include <gl2ext.h>
+#include <gl3.h>
+#include <gl3ext.h>
 
 #elif defined (IPHONE) || defined(APPLETV)
 
