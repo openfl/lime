f2a8092834bd53410fc3e8b241bb25c9b43a3ff1
Joshua Granick <jgranick@users.noreply.github.com>
2017-08-21 15:08:05 -0700

    gaussianBlur strength should be a Float

--- lime/graphics/utils/ImageDataUtil.hx
+++ lime/graphics/utils/ImageDataUtil.hx
@@ -503,7 +503,7 @@ class ImageDataUtil {
 	}
 	
 	
-	public static function gaussianBlur (image:Image, sourceImage:Image, sourceRect:Rectangle, destPoint:Vector2, blurX:Float = 4, blurY:Float = 4, quality:Int = 1, strength:Int = 1):Image {
+	public static function gaussianBlur (image:Image, sourceImage:Image, sourceRect:Rectangle, destPoint:Vector2, blurX:Float = 4, blurY:Float = 4, quality:Int = 1, strength:Float = 1):Image {
 		
 		// TODO: Support sourceRect better, do not modify sourceImage, create C++ implementation for native
 		
