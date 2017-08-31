1991cce87cedb7c1991d833e2474510018982b1f
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2017-08-31 14:38:09 -0400

    Use Content-Length for buffer on NativeHTTPRequest

--- lime/_backend/native/NativeHTTPRequest.hx
+++ lime/_backend/native/NativeHTTPRequest.hx
@@ -34,6 +34,7 @@ class NativeHTTPRequest {
 	private var parent:_IHTTPRequest;
 	private var promise:Promise<Bytes>;
 	private var readPosition:Int;
+	private var writePosition:Int;
 	
 	
 	public function new () {
@@ -174,6 +175,7 @@ class NativeHTTPRequest {
 		bytesLoaded = 0;
 		bytesTotal = 0;
 		readPosition = 0;
+		writePosition = 0;
 		
 		if (curl == 0) {
 			
@@ -350,7 +352,19 @@ class NativeHTTPRequest {
 		
 		parent.responseHeaders = [];
 		
-		// TODO
+		var parts = Std.string(output).split(': ');
+		if (parts.length == 2) {
+			switch (parts[0]) {
+				case 'Content-Length': 
+					var length = Std.parseInt(parts[1]);
+					if (length > bytes.length) {
+						var cacheBytes = bytes;
+						bytes = Bytes.alloc (length);
+						bytes.blit (0, cacheBytes, 0, cacheBytes.length);
+					}
+				// TODO
+			}
+		}
 		
 		return size * nmemb;
 		
@@ -406,10 +420,16 @@ class NativeHTTPRequest {
 	
 	private function curl_onWrite (output:Bytes, size:Int, nmemb:Int):Int {
 		
-		var cacheBytes = bytes;
-		bytes = Bytes.alloc (bytes.length + output.length);
-		bytes.blit (0, cacheBytes, 0, cacheBytes.length);
-		bytes.blit (cacheBytes.length, output, 0, output.length);
+		if (bytes.length < writePosition + output.length) {
+			var cacheBytes = bytes;
+			bytes = Bytes.alloc (writePosition + output.length);
+			bytes.blit (0, cacheBytes, 0, cacheBytes.length);
+			bytes.blit (writePosition, output, 0, output.length);
+		} else {
+			bytes.blit (writePosition, output, 0, output.length);
+		}
+
+		writePosition += output.length;
 		
 		return size * nmemb;
 		
