a454265cc4d73c252cf3fcff3254b027d35fa40d
Joshua Granick <jgranick@users.noreply.github.com>
2017-12-20 21:38:19 -0800

    Fix debug message when HXCPP std is statically linked

--- lime/system/CFFI.hx
+++ lime/system/CFFI.hx
@@ -265,12 +265,12 @@ class CFFI {
 	private static function __loaderTrace (message:String) {
 		
 		#if (sys && !html5)
-		#if cpp
-		var get_env = cpp.Lib.load ("std", "get_env", 1);
-		var debug = (get_env ("OPENFL_LOAD_DEBUG") != null);
-		#else
+		// #if (haxe_ver < 3.4)
+		// var get_env = cpp.Lib.load ("std", "get_env", 1);
+		// var debug = (get_env ("OPENFL_LOAD_DEBUG") != null);
+		// #else
 		var debug = (Sys.getEnv ("OPENFL_LOAD_DEBUG") !=null);
-		#end
+		// #end
 		
 		if (debug) {
 			
