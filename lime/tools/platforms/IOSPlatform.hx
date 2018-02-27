4879eecae485154f96b56b24cfddb126e9a5e640
Justin Espedal <justin.espedal@gmail.com>
2018-02-27 13:08:48 -0800

    Fix for haxelib path containing spaces

--- lime/tools/platforms/IOSPlatform.hx
+++ lime/tools/platforms/IOSPlatform.hx
@@ -373,7 +373,7 @@ class IOSPlatform extends PlatformTarget {
 		
 		if (haxelibPath != null) {
 			
-			context.HAXELIB_PATH = 'export HAXELIB_PATH=$haxelibPath;';
+			context.HAXELIB_PATH = 'export HAXELIB_PATH="$haxelibPath";';
 			
 		} else {
 			
