ba3709002256699e861f0dffa3001190a7b92795
Dan Korostelev <nadako@gmail.com>
2018-05-30 13:45:48 +0200

    Don't do any magical implicit to-canvas conversion in the Image.src getter.
    We want to access normal source value which can be HTML Image element, also suitable for texture uploading.
    If anyone needs a canvas, they can convert explicitly (as it's actually done already).

--- lime/graphics/Image.hx
+++ lime/graphics/Image.hx
@@ -1828,14 +1828,6 @@ class Image {
 	
 	private function get_src ():Dynamic {
 		
-		#if (js && html5)
-		if (buffer.__srcCanvas == null) {
-			
-			ImageCanvasUtil.convertToCanvas (this);
-			
-		}
-		#end
-		
 		return buffer.src;
 		
 	}
