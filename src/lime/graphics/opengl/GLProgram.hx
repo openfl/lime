911e0992404fd16ea91f9bfcc1c49b692cab9608
Joshua Granick <jgranick@users.noreply.github.com>
2018-07-04 23:25:24 -0700

    Change GLUtils to GLProgram/GLShader fromSource/fromSources

--- src/lime/graphics/opengl/GLProgram.hx
+++ src/lime/graphics/opengl/GLProgram.hx
@@ -1,17 +1,47 @@
-package lime.graphics.opengl; #if lime_opengl #if (!js || !html5 || display)
+package lime.graphics.opengl; #if lime_opengl
 
 
 import lime.graphics.opengl.GL;
+import lime.graphics.opengl.GLShader;
+import lime.graphics.WebGLRenderContext;
+import lime.utils.Log;
 
-@:forward(id, refs)
 
-
-abstract GLProgram(GLObject) from GLObject to GLObject {
+#if (!js || !html5 || display)
+@:forward(id, refs) abstract GLProgram(GLObject) from GLObject to GLObject {
+#else
+@:forward() abstract GLProgram(js.html.webgl.Shader) from js.html.webgl.Program to js.html.webgl.Program {
+#end
 	
 	
+	#if (!js || !html5 || display)
 	@:from private static function fromInt (id:Int):GLProgram {
 		
 		return GLObject.fromInt (PROGRAM, id);
+		
+	}
+	#end
+	
+	
+	public static function fromSources (gl:WebGLRenderContext, vertexSource:String, fragmentSource:String):GLProgram {
+		
+		var vertexShader = GLShader.fromSource (gl, vertexSource, gl.VERTEX_SHADER);
+		var fragmentShader = GLShader.fromSource (gl, fragmentSource, gl.FRAGMENT_SHADER);
+		
+		var program = gl.createProgram ();
+		gl.attachShader (program, vertexShader);
+		gl.attachShader (program, fragmentShader);
+		gl.linkProgram (program);
+		
+		if (gl.getProgramParameter (program, GL.LINK_STATUS) == 0) {
+			
+			var message = "Unable to initialize the shader program";
+			message += "\n" + GL.getProgramInfoLog (program);
+			Log.error (message);
+			
+		}
+		
+		return program;
 		
 	}
 	
@@ -19,9 +49,6 @@ abstract GLProgram(GLObject) from GLObject to GLObject {
 }
 
 
-#else
-typedef GLProgram = js.html.webgl.Program;
-#end
 #else
 typedef GLProgram = Dynamic;
 #end
--- src/lime/graphics/opengl/GLShader.hx
+++ src/lime/graphics/opengl/GLShader.hx
@@ -1,17 +1,47 @@
-package lime.graphics.opengl; #if lime_opengl #if (!js || !html5 || display)
+package lime.graphics.opengl; #if lime_opengl
 
 
 import lime.graphics.opengl.GL;
+import lime.graphics.WebGLRenderContext;
+import lime.utils.Log;
 
-@:forward(id)
 
-
-abstract GLShader(GLObject) from GLObject to GLObject {
+#if (!js || !html5 || display)
+@:forward(id) abstract GLShader(GLObject) from GLObject to GLObject {
+#else
+@:forward() abstract GLShader(js.html.webgl.Shader) from js.html.webgl.Shader to js.html.webgl.Shader {
+#end
 	
 	
+	#if (!js || !html5 || display)
 	@:from private static function fromInt (id:Int):GLShader {
 		
 		return GLObject.fromInt (SHADER, id);
+		
+	}
+	#end
+	
+	
+	public static function fromSource (gl:WebGLRenderContext, source:String, type:Int):GLShader {
+		
+		var shader = gl.createShader (type);
+		gl.shaderSource (shader, source);
+		gl.compileShader (shader);
+		
+		if (gl.getShaderParameter (shader, gl.COMPILE_STATUS) == 0) {
+			
+			var message;
+			
+			if (type == gl.VERTEX_SHADER) message = "Error compiling vertex shader";
+			else if (type == gl.FRAGMENT_SHADER) message = "Error compiling fragment shader";
+			else message = "Error compiling unknown shader type";
+			
+			message += "\n" + gl.getShaderInfoLog (shader);
+			Log.error (message);
+			
+		}
+		
+		return shader;
 		
 	}
 	
@@ -19,9 +49,6 @@ abstract GLShader(GLObject) from GLObject to GLObject {
 }
 
 
-#else
-typedef GLShader = js.html.webgl.Shader;
-#end
 #else
 typedef GLShader = Dynamic;
 #end
