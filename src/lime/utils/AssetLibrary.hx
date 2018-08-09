b86aea2893a3c56ad3c3b0fa67731ccece18ec1c
Joshua Granick <jgranick@users.noreply.github.com>
2018-08-09 12:02:19 -0700

    Minor improvement to AssetLibrary projgress events

--- src/lime/utils/AssetLibrary.hx
+++ src/lime/utils/AssetLibrary.hx
@@ -769,6 +769,7 @@ class AssetLibrary {
 		for (asset in manifest.assets) {
 
 			size = hasSize ? asset.size : 100;
+			if (size == null) size = 100;
 			id = asset.id;
 
 			if (Reflect.hasField (asset, "path")) {
