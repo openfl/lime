cb8c7aafd95dcb96d6557dc7047b10f6c2043c07
Joshua Granick <jgranick@users.noreply.github.com>
2018-08-15 13:39:18 -0700

    Fix support of WebGL2 in default HTML5 window

--- src/lime/_internal/backend/html5/HTML5Window.hx
+++ src/lime/_internal/backend/html5/HTML5Window.hx
@@ -297,7 +297,7 @@ class HTML5Window {
 
 			var forceCanvas = #if (canvas || munit) true #else (renderType == CANVAS) #end;
 			var forceWebGL = #if webgl true #else (renderType == OPENGL || renderType == OPENGLES || renderType == WEBGL) #end;
-			var allowWebGL2 = #if webgl1 false #else (forceWebGL && (!Reflect.hasField (contextAttributes, "version") || contextAttributes.version != "1")) #end;
+			var allowWebGL2 = #if webgl1 false #else (!Reflect.hasField (contextAttributes, "version") || contextAttributes.version != "1") #end;
 			var isWebGL2 = false;
 
 			if (forceWebGL || (!forceCanvas && (!Reflect.hasField (contextAttributes, "hardware") || contextAttributes.hardware))) {
@@ -308,7 +308,7 @@ class HTML5Window {
 				var options = {
 
 					alpha: (transparentBackground || colorDepth > 16) ? true : false,
-					antialias: Reflect.hasField (contextAttributes, "antialiasing") ? contextAttributes.antialiasing > 0 : false,
+					antialias: Reflect.hasField (contextAttributes, "antialiasing") ? false : false,
 					depth: Reflect.hasField (contextAttributes, "depth") ? contextAttributes.depth : true,
 					premultipliedAlpha: true,
 					stencil: Reflect.hasField (contextAttributes, "stencil") ? contextAttributes.stencil : false,
