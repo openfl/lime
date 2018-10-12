4a8125e03641d1dc7bf18b6e6d779b1d24e7d3cb
Joshua Granick <jgranick@users.noreply.github.com>
2018-10-12 14:30:38 -0700

    Support more HTML5 keycodes (based on wesbos/keycodes, resolve openfl/openfl#1575)

--- src/lime/_internal/backend/html5/HTML5Application.hx
+++ src/lime/_internal/backend/html5/HTML5Application.hx
@@ -66,9 +66,11 @@ class HTML5Application {
 
 		switch (keyCode) {
 
+			case 12: return KeyCode.CLEAR;
 			case 16: return KeyCode.LEFT_SHIFT;
 			case 17: return KeyCode.LEFT_CTRL;
 			case 18: return KeyCode.LEFT_ALT;
+			case 19: return KeyCode.PAUSE;
 			case 20: return KeyCode.CAPS_LOCK;
 			case 33: return KeyCode.PAGE_UP;
 			case 34: return KeyCode.PAGE_DOWN;
@@ -78,8 +80,15 @@ class HTML5Application {
 			case 38: return KeyCode.UP;
 			case 39: return KeyCode.RIGHT;
 			case 40: return KeyCode.DOWN;
+			case 41: return KeyCode.SELECT;
+			case 43: return KeyCode.EXECUTE;
+			case 44: return KeyCode.PRINT_SCREEN;
 			case 45: return KeyCode.INSERT;
 			case 46: return KeyCode.DELETE;
+			case 91: return KeyCode.LEFT_META;
+			case 92: return KeyCode.RIGHT_META;
+			case 93: return KeyCode.RIGHT_META; // this maybe should be APPLICATION if on Windows
+			case 95: return KeyCode.SLEEP;
 			case 96: return KeyCode.NUMPAD_0;
 			case 97: return KeyCode.NUMPAD_1;
 			case 98: return KeyCode.NUMPAD_2;
@@ -92,6 +101,7 @@ class HTML5Application {
 			case 105: return KeyCode.NUMPAD_9;
 			case 106: return KeyCode.NUMPAD_MULTIPLY;
 			case 107: return KeyCode.NUMPAD_PLUS;
+			case 108: return KeyCode.NUMPAD_PERIOD;
 			case 109: return KeyCode.NUMPAD_MINUS;
 			case 110: return KeyCode.NUMPAD_PERIOD;
 			case 111: return KeyCode.NUMPAD_DIVIDE;
@@ -110,18 +120,55 @@ class HTML5Application {
 			case 124: return KeyCode.F13;
 			case 125: return KeyCode.F14;
 			case 126: return KeyCode.F15;
+			case 127: return KeyCode.F16;
+			case 128: return KeyCode.F17;
+			case 129: return KeyCode.F18;
+			case 130: return KeyCode.F19;
+			case 131: return KeyCode.F20;
+			case 132: return KeyCode.F21;
+			case 133: return KeyCode.F22;
+			case 134: return KeyCode.F23;
+			case 135: return KeyCode.F24;
 			case 144: return KeyCode.NUM_LOCK;
-			case 186: return KeyCode.SEMICOLON;
+			case 145: return KeyCode.SCROLL_LOCK;
+			case 160: return KeyCode.CARET;
+			case 161: return KeyCode.EXCLAMATION;
+			case 163: return KeyCode.HASH;
+			case 164: return KeyCode.DOLLAR;
+			case 166: return KeyCode.APP_CONTROL_BACK;
+			case 167: return KeyCode.APP_CONTROL_FORWARD;
+			case 168: return KeyCode.APP_CONTROL_REFRESH;
+			case 169: return KeyCode.RIGHT_PARENTHESIS; // is this correct?
+			case 170: return KeyCode.ASTERISK;
+			case 171: return KeyCode.GRAVE;
+			case 172: return KeyCode.HOME;
+			case 173: return KeyCode.MINUS; // or mute/unmute?
+			case 174: return KeyCode.VOLUME_DOWN;
+			case 175: return KeyCode.VOLUME_UP;
+			case 176: return KeyCode.AUDIO_NEXT;
+			case 177: return KeyCode.AUDIO_PREVIOUS;
+			case 178: return KeyCode.AUDIO_STOP;
+			case 179: return KeyCode.AUDIO_PLAY;
+			case 180: return KeyCode.MAIL;
+			case 181: return KeyCode.AUDIO_MUTE;
+			case 182: return KeyCode.VOLUME_DOWN;
+			case 183: return KeyCode.VOLUME_UP;
+			case 186: return KeyCode.SEMICOLON; // or ñ?
 			case 187: return KeyCode.EQUALS;
 			case 188: return KeyCode.COMMA;
 			case 189: return KeyCode.MINUS;
 			case 190: return KeyCode.PERIOD;
 			case 191: return KeyCode.SLASH;
 			case 192: return KeyCode.GRAVE;
+			case 193: return KeyCode.QUESTION;
+			case 194: return KeyCode.NUMPAD_PERIOD;
 			case 219: return KeyCode.LEFT_BRACKET;
 			case 220: return KeyCode.BACKSLASH;
 			case 221: return KeyCode.RIGHT_BRACKET;
 			case 222: return KeyCode.SINGLE_QUOTE;
+			case 223: return KeyCode.GRAVE;
+			case 224: return KeyCode.LEFT_META;
+			case 226: return KeyCode.BACKSLASH;
 
 		}
 
