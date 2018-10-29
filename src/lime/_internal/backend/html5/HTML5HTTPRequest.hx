298d2b9ae644b242e22b3ff1c4a3b913a18cc396
loudoweb <loudoweb@gmail.com>
2018-10-29 23:52:38 +0100

    Add forceLocal define to allow loading locally
    Compiling with -DforceLocal allow files to be served locally when browser supports this.
    Use case : Cordova use a webview and many files can't be loaded without allowing http status 0 as successful.

--- src/lime/_internal/backend/html5/HTML5HTTPRequest.hx
+++ src/lime/_internal/backend/html5/HTML5HTTPRequest.hx
@@ -42,7 +42,7 @@ class HTML5HTTPRequest {
 
 	public function new () {
 
-		validStatus0 = ~/Tizen/gi.match (Browser.window.navigator.userAgent);
+		validStatus0 = #if forceLocal true #else ~/Tizen/gi.match (Browser.window.navigator.userAgent) #end;
 
 	}
 
