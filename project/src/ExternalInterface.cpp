aa1e7c051d39744d756bce80de484134d59361fc
Lars Doucet <lars.doucet@gmail.com>
2019-01-09 13:22:46 -0600

    fix compiling on mac

--- project/src/ExternalInterface.cpp
+++ project/src/ExternalInterface.cpp
@@ -2,6 +2,10 @@
 #define IMPLEMENT_API
 #endif
 
+#if defined(HX_MACOS)
+#include <cstdlib>
+#endif
+
 #if defined(HX_WINDOWS) || defined(HX_MACOS) || defined(HX_LINUX)
 #define NEKO_COMPATIBLE
 #endif
