d9dbea7f6bbfa97a853b98f81bf169bcc0573dd0
Joshua Granick <jgranick@users.noreply.github.com>
2019-02-07 16:43:42 -0800

    Try a hidden ApplicationMain.hx file instead

---
+++ src/ApplicationMain.hx
@@ -0,0 +1,11 @@
+package;
+
+/**
+	Help improve code completion for projects that
+	have not run `lime update` yet
+**/
+@:dox(hide) @:noCompletion class ApplicationMain {
+
+	public static function main () {}
+
+}
