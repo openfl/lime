a22d55762a7bc17f34bd624ce316f91371bbf5f6
Joshua Granick <jgranick@users.noreply.github.com>
2020-12-17 12:37:11 -0800

    Do not fail if major performance caveat and WebGL was requested

--- src/lime/_internal/backend/html5/HTML5Window.hx
+++ src/lime/_internal/backend/html5/HTML5Window.hx
@@ -287,7 +287,7 @@ class HTML5Window
 						premultipliedAlpha: true,
 						stencil: Reflect.hasField(contextAttributes, "stencil") ? contextAttributes.stencil : false,
 						preserveDrawingBuffer: false,
-						failIfMajorPerformanceCaveat: true
+						failIfMajorPerformanceCaveat: false
 					};
 
 				var glContextType = ["webgl", "experimental-webgl"];
