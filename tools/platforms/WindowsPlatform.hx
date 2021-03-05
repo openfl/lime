4271854980378bdb54f4470b0996335869619837
Josh Tynjala <joshtynjala@bowlerhat.dev>
2021-03-05 11:35:43 -0800

    WindowsPlatform: warn instead of error if HL_PATH is not defined

--- tools/platforms/WindowsPlatform.hx
+++ tools/platforms/WindowsPlatform.hx
@@ -209,7 +209,7 @@ class WindowsPlatform extends PlatformTarget
 						{
 							var command = #if lime "lime" #else "hxp" #end;
 
-							Log.error("You must define HL_PATH to copy HashLink dependencies, please run '" + command + " setup hl' first");
+							Log.warn("You must define HL_PATH to copy HashLink dependencies, please run '" + command + " setup hl' first");
 
 						}else{
 							System.copyFile(project.environment.get("HL_PATH") + '/ssl.hdll', applicationDirectory + '/ssl.hdll');
