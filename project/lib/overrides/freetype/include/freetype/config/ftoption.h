e1c55103a675132f81169375aebb663a01bd9c6c
Joseph Cloutier <jkcloutier@gmail.com>
2022-05-21 20:28:16 -0400

    Update FreeType to 2.12.1.
    Note: cff.c includes all the other cffxyz.c files. They're still there.

--- project/lib/overrides/freetype/include/freetype/config/ftoption.h
+++ project/lib/overrides/freetype/include/freetype/config/ftoption.h
@@ -218,7 +218,7 @@ FT_BEGIN_HEADER
   /*   options set by those programs have precendence, overwriting the     */
   /*   value here with the configured one.                                 */
   /*                                                                       */
-/* #define FT_CONFIG_OPTION_SYSTEM_ZLIB */
+#define FT_CONFIG_OPTION_SYSTEM_ZLIB
 
 
   /*************************************************************************/
