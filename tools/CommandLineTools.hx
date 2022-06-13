83b39e918443d97bb4a99839e7a691e2ad644664
Joseph Cloutier <jkcloutier@gmail.com>
2022-06-13 15:57:23 -0400

    Don't treat include.hxp as a complete project.

--- tools/CommandLineTools.hx
+++ tools/CommandLineTools.hx
@@ -1179,7 +1179,7 @@ class CommandLineTools
 					if ((extension == "lime" && file != "include.lime")
 						|| (extension == "nmml" && file != "include.nmml")
 						|| (extension == "xml" && file != "include.xml")
-						|| extension == "hxp")
+						|| (extension == "hxp" && file != "include.hxp"))
 					{
 						matches.get(extension).push(path);
 					}
