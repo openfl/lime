2903975465e72bedb861702ce6d4c03eb04f7aa5
player-03 <player3.14@gmail.com>
2022-08-12 12:22:27 -0400

    Sort functions.

--- src/lime/_internal/backend/html5/HTML5Window.hx
+++ src/lime/_internal/backend/html5/HTML5Window.hx
@@ -378,6 +378,11 @@ class HTML5Window
 		return false;
 	}
 
+	public function getOpacity():Float
+	{
+		return 1.0;
+	}
+
 	public function getTextInputEnabled():Bool
 	{
 		return textInputEnabled;
@@ -1085,6 +1090,8 @@ class HTML5Window
 
 	public function setMouseLock(value:Bool):Void {}
 
+	public function setOpacity(value:Float):Void {}
+
 	public function setResizable(value:Bool):Bool
 	{
 		return value;
@@ -1291,8 +1298,4 @@ class HTML5Window
 	}
 
 	public function warpMouse(x:Int, y:Int):Void {}
-
-	public function getOpacity():Float { return 1.0; }
-
-	public function setOpacity(value:Float):Void {}
 }
