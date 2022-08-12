6b2724b147c636673d00738b45c1d6f25745184e
player-03 <player3.14@gmail.com>
2022-08-12 12:41:43 -0400

    Use getters and setters for `Window.opacity`.

--- src/lime/ui/Window.hx
+++ src/lime/ui/Window.hx
@@ -80,6 +80,7 @@ class Window
 	public var onRestore(default, null) = new Event<Void->Void>();
 	public var onTextEdit(default, null) = new Event<String->Int->Int->Void>();
 	public var onTextInput(default, null) = new Event<String->Void>();
+	public var opacity(get, set):Float;
 	public var parameters:Dynamic;
 	public var resizable(get, set):Bool;
 	public var scale(get, null):Float;
@@ -413,14 +414,6 @@ class Window
 		__backend.warpMouse(x, y);
 	}
 
-	public function getOpacity():Float {
-		return __backend.getOpacity();
-	}
-
-	public function setOpacity(opacity:Float):Void {
-		__backend.setOpacity(opacity);
-	}
-
 	// Get & Set Methods
 	@:noCompletion private function get_cursor():MouseCursor
 	{
@@ -523,6 +516,17 @@ class Window
 	@:noCompletion private function set_mouseLock(value:Bool):Bool
 	{
 		__backend.setMouseLock(value);
+		return value;
+	}
+
+	@:noCompletion private function get_opacity():Float
+	{
+		return __backend.getOpacity();
+	}
+
+	@:noCompletion private function set_opacity(value:Float):Float
+	{
+		__backend.setOpacity(value);
 		return value;
 	}
 
