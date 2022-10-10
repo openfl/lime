88004be15460d418fb778106b7cc71d9ae72198a
player-03 <player3.14@gmail.com>
2022-10-10 13:34:23 -0400

    Fix typo.

--- src/lime/ui/Window.hx
+++ src/lime/ui/Window.hx
@@ -57,7 +57,7 @@ class Window
 	public var onClose(default, null) = new Event<Void->Void>();
 	public var onDeactivate(default, null) = new Event<Void->Void>();
 	public var onDropFile(default, null) = new Event<String->Void>();
-	// SDL_DROPTEXT is only iimplemented for X11 on Linux
+	// SDL_DROPTEXT is only implemented for X11 on Linux
 	//public var onDropText(default, null) = new Event<String->Void>();
 	public var onDropStart(default, null) = new Event<Void->Void>();
 	public var onDropEnd(default, null) = new Event<Void->Void>();
