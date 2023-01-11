e03cc18d31392b40f8079c4d53b17830abbc9dca
Josh Tynjala <joshtynjala@bowlerhat.dev>
2023-01-11 15:59:49 -0800

    HTML5Window: use HTMLInputElement password instead of text to avoid IME issues on Android (closes openfl/openfl#2564)

--- src/lime/_internal/backend/html5/HTML5Window.hx
+++ src/lime/_internal/backend/html5/HTML5Window.hx
@@ -1115,7 +1115,8 @@ class HTML5Window
 			if (textInput == null)
 			{
 				textInput = cast Browser.document.createElement('input');
-				textInput.type = 'text';
+				// use password instead of text to avoid IME issues on Android
+				textInput.type = 'password';
 				textInput.style.position = 'absolute';
 				textInput.style.opacity = "0";
 				textInput.style.color = "transparent";
