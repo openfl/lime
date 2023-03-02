9b9d433ce80b59cb9c758e2c08e64ca212feff2b
Joseph Cloutier <player3.14@gmail.com>
2023-03-02 14:30:23 -0500

    Add deprecation warning to `BackgroundWorker`.
    I also reworded it, focusing on what to do.

--- src/lime/system/BackgroundWorker.hx
+++ src/lime/system/BackgroundWorker.hx
@@ -1,3 +1,4 @@
 package lime.system;
 
+@:deprecated("Replace references to lime.system.BackgroundWorker with lime.system.ThreadPool. As the API is identical, no other changes are necessary.")
 typedef BackgroundWorker = ThreadPool;
