21b502ab172b2ad4c9677d7ee80ce21da47043ee
Josh Tynjala <joshtynjala@bowlerhat.dev>
2023-04-27 07:58:36 -0700

    Window: when calling setMinSize() or setMaxSize(), ensure that width and height are updated in the backend by calling resize() instead of setting __width and __height

--- src/lime/ui/Window.hx
+++ src/lime/ui/Window.hx
@@ -437,11 +437,8 @@ class Window
 
 		__minWidth = width;
 		__minHeight = height;
-		if (__width < __minWidth) {
-			__width = __minWidth;
-		}
-		if (__height < __minHeight) {
-			__height = __minHeight;
+		if (__width < __minWidth || __height < __minHeight) {
+			resize(__width, __height);
 		}
 	}
 
@@ -451,11 +448,8 @@ class Window
 
 		__maxWidth = width;
 		__maxHeight = height;
-		if (__width > __maxWidth) {
-			__width = __maxWidth;
-		}
-		if (__height > __maxHeight) {
-			__height = __maxHeight;
+		if (__width > __maxWidth || __height > __maxHeight) {
+			resize(__width, __height);
 		}
 	}
 
