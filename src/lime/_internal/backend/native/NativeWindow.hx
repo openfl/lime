f808e4513207833b1869355718c1ad531f9547bc
Josh Tynjala <joshtynjala@bowlerhat.dev>
2023-05-02 08:28:46 -0700

    Fix more :enum abstract warnings in Haxe 4.3

--- src/lime/_internal/backend/native/NativeWindow.hx
+++ src/lime/_internal/backend/native/NativeWindow.hx
@@ -665,7 +665,7 @@ class NativeWindow
 	}
 }
 
-@:enum private abstract MouseCursorType(Int) from Int to Int
+#if (haxe_ver >= 4.0) private enum #else @:enum private #end abstract MouseCursorType(Int) from Int to Int
 {
 	var HIDDEN = 0;
 	var ARROW = 1;
@@ -682,7 +682,7 @@ class NativeWindow
 	var WAIT_ARROW = 12;
 }
 
-@:enum private abstract WindowFlags(Int)
+#if (haxe_ver >= 4.0) private enum #else @:enum private #end abstract WindowFlags(Int)
 {
 	var WINDOW_FLAG_FULLSCREEN = 0x00000001;
 	var WINDOW_FLAG_BORDERLESS = 0x00000002;
