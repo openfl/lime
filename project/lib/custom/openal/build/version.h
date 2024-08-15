4fa0018885d8240c2b24f4114a43b7f4cfa2acc4
ACrazyTown <luigibros626@gmail.com>
2024-08-15 20:32:33 +0200

    Update OpenAL Soft to 1.22.2 & set HXCPP to use C++17 standard

--- project/lib/custom/openal/build/version.h
+++ project/lib/custom/openal/build/version.h
@@ -1,8 +1,8 @@
 /* Define to the library version */
-#define ALSOFT_VERSION "1.19.0"
+#define ALSOFT_VERSION "1.22.2"
 
 /* Define the branch being built */
 #define ALSOFT_GIT_BRANCH "master"
 
 /* Define the hash of the head commit */
-#define ALSOFT_GIT_COMMIT_HASH "96aacac"
+#define ALSOFT_GIT_COMMIT_HASH "dc83d99"
