24f31a46db7bb2fc733254a34260888fa318bad8
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-09-28 21:34:56 +0300

    Oops

--- project/src/backend/sdl/SDLApplication.cpp
+++ project/src/backend/sdl/SDLApplication.cpp
@@ -329,7 +329,7 @@ namespace lime {
 	void SDLApplication::Init () {
 
 		active = true;
-		lastUpdate = SDL_GetTicks ();
+		lastUpdate = ((double) SDL_GetPerformanceCounter () / (double) SDL_GetPerformanceFrequency ()) * 1000.0;
 		nextUpdate = lastUpdate;
 
 	}
