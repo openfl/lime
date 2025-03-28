8e8ac22034bc84e5d7f1273ffacb73646f795ae9
Fancy2209 <64917206+Fancy2209@users.noreply.github.com>
2025-03-28 16:52:30 -0100

    fix fread and fwrite

--- project/src/backend/sdl3/SDLSystem.cpp
+++ project/src/backend/sdl3/SDLSystem.cpp
@@ -916,8 +916,9 @@ namespace lime {
 		System::GCEnterBlocking ();
 
 		#ifndef HX_WINDOWS
-
-		nmem = SDL_ReadIO (stream ? (SDL_IOStream*)stream->handle : NULL, ptr, size*count);
+  if(size > 0 && count > 0)
+	  	nmem = SDL_ReadIO (stream ? (SDL_IOStream*)stream->handle : NULL, ptr, size * count) / size;
+  else nmem = 0;
 
 		#else
 
@@ -979,8 +980,9 @@ namespace lime {
 		System::GCEnterBlocking ();
 
 		#ifndef HX_WINDOWS
-
-		nmem = SDL_WriteIO (stream ? (SDL_IOStream*)stream->handle : NULL, ptr, size*count);
+  if(size > 0 && count > 0)
+		  nmem = SDL_WriteIO (stream ? (SDL_IOStream*)stream->handle : NULL, ptr, size *count) / size;
+  else nmem = 0;
 
 		#else
 
