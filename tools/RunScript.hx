205eacc1571bdcc70350c5917f517465c4188828
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2025-10-21 18:13:36 +0300

    Fix for `Eval`.

--- tools/RunScript.hx
+++ tools/RunScript.hx
@@ -212,6 +212,7 @@ class RunScript
 				+ "` using Eval (https://haxe.org/blog/eval/)");
 
 			var args = [
+				"--cwd", limeDirectory,
 				"-D", "lime",
 				"-cp", toolsDirectory,
 				"-cp", Path.combine(toolsDirectory, "platforms"),
