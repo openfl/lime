b243b8ce15255ffd9b479da98641b1a26941dee4
barisyild <fb_bcy@hotmail.com>
2025-12-09 02:02:34 +0300

    remove debug artifact

--- project/src/ExternalInterface.cpp
+++ project/src/ExternalInterface.cpp
@@ -4421,9 +4421,7 @@ extern "C" int lime_vorbis_register_prims () { return 0; }
 extern "C" int lime_register_prims () {
 
 	lime_cairo_register_prims ();
-	#ifndef LIME_CURL
 	lime_curl_register_prims ();
-	#endif
 	lime_harfbuzz_register_prims ();
 	lime_openal_register_prims ();
 	lime_opengl_register_prims ();
