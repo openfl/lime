92ef48286509ece75a1a291dbc792fa4563ab265
Rainy <939029835@qq.com>
2026-01-12 09:32:36 +0800

    Enable IME UI and extended text support

--- project/src/backend/sdl/SDLWindow.cpp
+++ project/src/backend/sdl/SDLWindow.cpp
@@ -75,6 +75,8 @@ namespace lime {
 		SDL_SetHint (SDL_HINT_MOUSE_FOCUS_CLICKTHROUGH, "1");
 		SDL_SetHint (SDL_HINT_MOUSE_TOUCH_EVENTS, "0");
 		SDL_SetHint (SDL_HINT_TOUCH_MOUSE_EVENTS, "1");
+		SDL_SetHint (SDL_HINT_IME_SHOW_UI, "1");
+        SDL_SetHint (SDL_HINT_IME_SUPPORT_EXTENDED_TEXT, "1");
 		#endif
 
 		if (flags & WINDOW_FLAG_HARDWARE) {
