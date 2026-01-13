openfl/lime

commit 22032ef6b51ed9e3e0f4c865cf65fd9d515abaa3

Allow eval for HXP projects.
Plus, do some cleanup. For instance, `nekoOutput` is only used in one
spot, so might as well declare it there.

Aside: I doubt `fullPath()` is needed, but I don't care to test it.

src/lime/tools/HXProject.hx
@@ -376,38 +376,18 @@ class HXProject extends Script
 
 		var tempDirectory = System.getTemporaryDirectory();
 		var classFile = Path.combine(tempDirectory, name + ".hx");
-		var nekoOutput = Path.combine(tempDirectory, name + ".n");
 
 		System.copyFile(path, classFile);
 
-		#if lime
 		var args = [
 			name,
-			"-main",
-			"lime.tools.HXProject",
-			"-cp",
-			tempDirectory,
-			"-neko",
-			nekoOutput,
-			"-cp",
-			Path.combine(Haxelib.getPath(new Haxelib("hxp")), "src"),
-			"-lib",
-			"lime",
-			"-lib",
-			"hxp"
+			#if lime
+			"-lib", "lime",
+			"-lib", "hxp",
+			#end
+			"-cp", tempDirectory,
+			"-cp", Path.combine(Haxelib.getPath(new Haxelib("hxp")), "src")
 		];
-		#else
-		var args = [
-			name,
-			"--interp",
-			"-main",
-			"lime.tools.HXProject",
-			"-cp",
-			tempDirectory,
-			"-cp",
-			Path.combine(Haxelib.getPath(new Haxelib("hxp")), "src")
-		];
-		#end
 		var input = File.read(classFile, false);
 		var tag = "@:compiler(";
 
@@ -429,10 +409,6 @@ class HXProject extends Script
 
 		var cacheDryRun = System.dryRun;
 		System.dryRun = false;
-
-		#if lime
-		System.runCommand("", "haxe", args);
-		#end
 
 		var inputFile = Path.combine(tempDirectory, "input.dat");
 		var outputFile = Path.combine(tempDirectory, "output.dat");
@@ -457,10 +433,12 @@ class HXProject extends Script
 
 		try
 		{
-			#if lime
-			System.runCommand("", "neko", [FileSystem.fullPath(nekoOutput), inputFile, outputFile]);
+			#if (lime && !eval)
+			var nekoOutput = FileSystem.fullPath(Path.combine(tempDirectory, name + ".n"));
+			System.runCommand("", "haxe", args.concat(["--main", "lime.tools.HXProject", "-neko", nekoOutput]));
+			System.runCommand("", "neko", [nekoOutput, inputFile, outputFile]);
 			#else
-			System.runCommand("", "haxe", args.concat(["--", inputFile, outputFile]));
+			System.runCommand("", "haxe", args.concat(["--run", "lime.tools.HXProject", inputFile, outputFile]));
 			#end
 		}
 		catch (e:Dynamic)

commit 809da7c48bb12f3258a57d1aa0a1b45e02eda4cf

[hxp] Fix compatbility with haxe 3
haxe 3 compatibility with hxp projects was broken in:
22032ef6b51ed9e3e0f4c865cf65fd9d515abaa3

This is because haxe 3 doesn't support --main, only -main.

src/lime/tools/HXProject.hx
@@ -452,7 +452,7 @@ class HXProject extends Script
 		{
 			#if (lime && !eval)
 			var nekoOutput = Path.combine(tempDirectory, name + ".n");
-			System.runCommand("", "haxe", args.concat(["--main", "lime.tools.HXProject", "-neko", nekoOutput]));
+			System.runCommand("", "haxe", args.concat(["-main", "lime.tools.HXProject", "-neko", nekoOutput]));
 			System.runCommand("", "neko", [nekoOutput, inputFile, outputFile]);
 			#else
 			System.runCommand("", "haxe", args.concat(["--run", "lime.tools.HXProject", inputFile, outputFile]));