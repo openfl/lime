c44ec4423f68a364db778c9120e3b78b5c155cd3
Joseph Cloutier <player3.14@gmail.com>
2026-03-02 19:08:38 -0500

    Unwrap `$lime_init` function.
    Three layers of nesting is too many, adding confusion for minimal benefit.
    
    Additionally, there's no need to calculate and pass `$globals`, when Haxe already runs the exact same calculation.

--- templates/html5/output.js
+++ templates/html5/output.js
@@ -1,4 +1,4 @@
-var $lime_init = (function ($hx_exports, $global) { "use strict"; var $hx_script = (function (exports, global) { ::SOURCE_FILE::
+"use strict"; var $hx_script = (function (exports) { ::SOURCE_FILE::
 });::if false::
 /*
 	Don't insert or remove any line breaks in the code above this line!
@@ -12,34 +12,31 @@ var $lime_init = (function ($hx_exports, $global) { "use strict"; var $hx_script
 	to avoid it getting ignored in a // comment at the end of ::SOURCE_FILE::.
 */
 ::end::
-	if (typeof self !== "undefined" && self.constructor.name.includes("Worker")) {
-		// No need for exports in a worker context, just initialize statics.
-		$hx_script({}, $global);
-	} else {
-		$hx_exports.lime = $hx_exports.lime || {};
-		$hx_exports.lime.$scripts = $hx_exports.lime.$scripts || {};
-		$hx_exports.lime.$scripts["::APP_FILE::"] = $hx_script;
-		$hx_exports.lime.embed = function (projectName) {
-			var exports = {};
-			var script = $hx_exports.lime.$scripts[projectName];
-			if (!script) throw Error("Cannot find project name \"" + projectName + "\"");
-			script(exports, $global);
-			for (var key in exports) $hx_exports[key] = $hx_exports[key] || exports[key];
-			var lime = exports.lime || window.lime;
-			if (lime && lime.embed && this !== lime.embed) lime.embed.apply(lime, arguments);
-			return exports;
-		};
-	}
+var $hx_exports = typeof exports !== "undefined" ? exports : typeof define === "function" && define.amd ? {} : typeof window !== "undefined" ? window : typeof self !== "undefined" ? self : this;
+if (typeof self !== "undefined" && self.constructor.name.includes("Worker")) {
+	// No need for exports in a worker context, just initialize statics.
+	$hx_script({});
+} else {
+	$hx_exports.lime = $hx_exports.lime || {};
+	$hx_exports.lime.$scripts = $hx_exports.lime.$scripts || {};
+	$hx_exports.lime.$scripts["::APP_FILE::"] = $hx_script;
+	$hx_exports.lime.embed = function (projectName) {
+		var exports = {};
+		var script = $hx_exports.lime.$scripts[projectName];
+		if (!script) throw Error("Cannot find project name \"" + projectName + "\"");
+		script(exports);
+		for (var key in exports) $hx_exports[key] = $hx_exports[key] || exports[key];
+		var lime = exports.lime || window.lime;
+		if (lime && lime.embed && this !== lime.embed) lime.embed.apply(lime, arguments);
+		return exports;
+	};
+}
 
-	if (typeof define === "function" && define.amd) {
-		define([], function () { return $hx_exports.lime; });
-		define.__amd = define.amd;
-		define.amd = null;
-	}
-})
-
-$lime_init(typeof exports !== "undefined" ? exports : typeof define === "function" && define.amd ? {} : typeof window !== "undefined" ? window : typeof self !== "undefined" ? self : this,
-typeof window !== "undefined" ? window : typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : this);
+if (typeof define === "function" && define.amd) {
+	define([], function () { return $hx_exports.lime; });
+	define.__amd = define.amd;
+	define.amd = null;
+}
 
 ::if embeddedLibraries::::foreach embeddedLibraries::
 ::__current__::::end::::end::
