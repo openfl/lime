6c871d15eed225dfc7e7e146b316915be0494cfa
Rafi <mohamedrafikhan2009@gmail.com>
2026-05-30 12:10:20 +0100

    NativeAplication.hx: Fix the error about EventLoop missing `progress``

--- src/lime/_internal/backend/native/NativeApplication.hx
+++ src/lime/_internal/backend/native/NativeApplication.hx
@@ -689,7 +689,15 @@ class NativeApplication
 				});
 			}
 		}
-		#if (haxe_ver >= 4.2)
+		#if haxe5
+		#if target.threaded
+		sys.thread.Thread.current().events.loopOnce();
+		#else
+		// Duplicate code required because Haxe 3 can't handle
+		// #if (haxe_ver >= 4.2 && target.threaded)
+		@:privateAccess haxe.EntryPoint.processEvents();
+		#end
+		#elseif (haxe_ver >= 4.2)
 		#if target.threaded
 		sys.thread.Thread.current().events.progress();
 		#else
